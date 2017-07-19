9463a161a1ba73fff45eac25b2e17b6d3d062a45
Simon Crase <simon@greenweaves.nz>
2017-07-20 09:05:41 +1200

    Simplified get_path

--- nwck.py
+++ nwck.py
@@ -3,16 +3,18 @@ import newick
 tokenizer = newick.Tokenizer()
 parser = newick.Parser(tokenizer)
 
-def get_ancestors(clade,path=[]):
+def get_path_to_root(clade,path=[]):
+    if (len(path)==0):
+        path=[clade.id]
     ancestor = clade.parent
     if ancestor == None:
         return path
     pp=path[:]
     pp.append(ancestor.id)
-    return get_ancestors(ancestor,pp)
+    return get_path_to_root(ancestor,pp)
 
 def get_path(clade):
-    return [clade.id]+get_ancestors(clade)
+    return [clade.id]+get_path_to_root(clade)
 
 def diff(path1,path2):
     if len(path1)>len(path2):
@@ -36,7 +38,7 @@ with open (r'C:\Users\Weka\Downloads\rosalind_nwck(1).txt') as f:
         elif i%3==1:
             clades = line.strip().split()
             print (clades)
-            ancestors=[get_path(lookup[clade])[::-1] for clade in clades]
+            ancestors=[get_path_to_root(lookup[clade])[::-1] for clade in clades]
             diffs.append(diff(ancestors[0],ancestors[1]))
         i+=1
     print (diffs)
