6bc1349c699fd92da30d284a35fc0c801217842c
Simon Crase <simon@greenweaves.nz>
2017-07-20 09:15:29 +1200

    Selved nkew: Newick Format with Edge Weights

--- nwck.py
+++ nwck.py
@@ -5,28 +5,30 @@ parser = newick.Parser(tokenizer)
 
 def get_path_to_root(clade,path=[]):
     if (len(path)==0):
-        path=[clade.id]
+        path=[(clade.id,clade.length)]
     ancestor = clade.parent
     if ancestor == None:
         return path
     pp=path[:]
-    pp.append(ancestor.id)
+    pp.append((ancestor.id,ancestor.length))
     return get_path_to_root(ancestor,pp)
 
 def get_path(clade):
     return [clade.id]+get_path_to_root(clade)
 
 def diff(path1,path2):
+    def length(path):
+        return sum(l for (_,l) in path)
     if len(path1)>len(path2):
         return diff(path2,path1)
     
     i=0
-    while i<len(path1)  and path1[i]==path2[i]:
+    while i<len(path1)  and path1[i][0]==path2[i][0]:
         i+=1
  
-    return len(path1[i:]) +len(path2[i:])
+    return length(path1[i:]) +length(path2[i:])
     
-with open (r'C:\Users\Weka\Downloads\rosalind_nwck(1).txt') as f:
+with open (r'C:\Users\Weka\Downloads\rosalind_nkew.txt') as f:
     diffs=[]
     i = 0
     tree = None
