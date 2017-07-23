86ed3b2ab9378938511bb2e17c17169b9a66aa54
Simon Crase <simon@greenweaves.nz>
2017-07-23 15:02:27 +1200

    Solved MEND: Inferring Genotype from a Pedigree

--- mend.py
+++ mend.py
@@ -13,13 +13,11 @@ factors=[
 ]
 
 def combine(f1,f2):
-    #print("combine ",f1,f2)
     def total(i,contribs):
         return sum(c[i] for c in contribs)
     contribs=[]
     for i in range(3):
         for j in range(3):
-            #print ("F",factors[i][j],f1[i]*f2[j])
             contribs.append([f*f1[i]*f2[j] for f in factors[i][j]])
     return [total(k,contribs) for k in range(3)]
 
@@ -29,16 +27,12 @@ def mend(node):
             if node.name=='aA':
                 node.name=node.name[::-1]
             freqs=frequencies[node.name]
-            #print ("NN",node.name,freqs)
             return freqs
         except KeyError:
             return (0,0)
     parent_freqs = [mend(parent) for parent in node.nodes]
-    #print("PF",parent_freqs)
     parent_freqs=[pp for pp in parent_freqs if len(pp)==3]
-    combined= combine(parent_freqs[0],parent_freqs[1])
-    #print ("C",combined)
-    return combined
+    return combine(parent_freqs[0],parent_freqs[1])
 
 tokenizer = newick.Tokenizer()
 parser = newick.Parser(tokenizer)
