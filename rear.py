dcfb8508952eaabfcc19f13ee367f8b21a82dc52
Simon Crase <simon@greenweaves.nz>
2017-07-24 21:39:03 +1200

    REAR Reversal Distance - correct, but takes 1794 seconds

--- rear.py
+++ rear.py
@@ -1,54 +1,42 @@
 '''REAR 	Reversal Distance '''
+import time
+
 
 def rear(s1,s2):
-    print (s1,s2)
-    #def cycle(s,offset):
-        #return s[offset:]+s[:offset]
-    #def normalize(s,start=1):
-        #return cycle(s,s.index(start))
     def reverse(s,i0,i1):
         if i0<i1:
             return s[:i0] + s[i0:i1][::-1] + s[i1:]
-        #else:
-            #return reverse(cycle(s,i0),0,i1+len(s)-i0)
-    #def cyclically_equal(s1,s2):
-        #i=s2.index(s1[0])
-        #return s2==cycle(s1,-i)
     def generate_single_reversals(s):
         return [reverse(s,i0,i1) for i0 in range(len(s)) for i1 in range(i0+1,len(s)+1)]
-    #target=s1#normalize(s1)
     if s1==s2:
         return 0
     history=set()
-    history.add(str(s2)) #history.add(str(normalize(s2))) # 3628800
+    history.add(str(s2))
     candidates=[s2]
     for depth in range(25):
-        print (depth,len(history))
+        print (depth, len(candidates), len(history))
+        for s in candidates:
+            if s1==s:
+                return depth        
         next_generation=[]
         for s in candidates:
-            #if s1==s: #normalize(s):
-                #return depth
             for rr in generate_single_reversals(s):
-                key= str(rr) #str(normalize(rr))
+                key= str(rr)
                 if not key in history:
                     next_generation.append(rr)
                     history.add(key)
-        for s in next_generation:
-            if s1==s:
-                return depth            
+        #for s in next_generation:
+            #if s1==s:
+                #return depth            
         candidates=next_generation
     return None
 
 def parse(line):
     return [int(c) for c in line.strip().split()]
 
-#rear ([1,2,3,4,5],[1,2,3,4,5])
-#rear ([1,2,3,4,5],[3,4,5,1,2])
-#rear ([1,2,3,4,5],[3,4,5,2,1])
+start_time = time.time()
 i=0
-
 original=''
-
 result=[]
 
 #with open (r'C:\Users\Weka\Downloads\rosalind_rear.txt') as f:
@@ -58,6 +46,7 @@ with open (r'rear.txt') as f:
             original=parse(line)
         if i%3==1:
             result.append(rear(original,parse(line)))
+            print("--- {0} seconds ---".format(time.time() - start_time))
         i+=1
     
 print(result)
