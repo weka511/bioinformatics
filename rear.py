9b5b69e0a331b2e81e9b908de3c0e189776db3de
Simon Crase <simon@greenweaves.nz>
2017-07-24 14:41:31 +1200

    Under test - desperately slow

--- rear.py
+++ rear.py
@@ -1,16 +1,59 @@
-input = r'''
-1 2 3 4 5 6 7 8 9 10
-3 1 5 2 7 4 9 6 10 8
+'''REAR 	Reversal Distance '''
 
-3 10 8 2 5 4 7 1 6 9
-5 2 3 1 7 4 10 8 6 9
+def rear(s1,s2):
+    print (s1,s2)
+    def cycle(s,offset):
+        return s[offset:]+s[:offset]
+    def normalize(s,start=1):
+        return cycle(s,s.index(start))
+    def reverse(s,i0,i1):
+        if i0<i1:
+            return s[:i0] + s[i0:i1][::-1] + s[i1:]
+        else:
+            return reverse(cycle(s,i0),0,i1+len(s)-i0)
+    def cyclically_equal(s1,s2):
+        i=s2.index(s1[0])
+        return s2==cycle(s1,-i)
+    def generate_single_reversals(s):
+        return [reverse(s,i0,i1) for i0 in range(len(s)) for i1 in range(len(s)) if i0!=i1]
+    target=normalize(s1)
 
-8 6 7 9 4 1 3 10 2 5
-8 2 7 6 9 1 5 3 10 4
+    history=set(str(normalize(s1)))
+    candidates=[s2]
+    for depth in range(25):
+        print (depth)
+        next_generation=[]
+        for s in candidates:
+            if target==normalize(s):
+                return depth
+            for rr in generate_single_reversals(s):
+                key= str(normalize(rr))
+                if not key in history:
+                    next_generation.append(rr)
+                    history.add(key)
+        candidates=next_generation
+    return None
 
-3 9 10 4 1 8 6 7 5 2
-2 9 8 5 1 7 3 4 6 10
+def parse(line):
+    return [int(c) for c in line.strip().split()]
 
-1 2 3 4 5 6 7 8 9 10
-1 2 3 4 5 6 7 8 9 10
-'''
+#rear ([1,2,3,4,5],[1,2,3,4,5])
+#rear ([1,2,3,4,5],[3,4,5,1,2])
+#rear ([1,2,3,4,5],[3,4,5,2,1])
+i=0
+
+original=''
+
+result=[]
+
+
+with open (r'rear.txt') as f:
+    for line in f:
+        if i%3==0:
+            original=parse(line)
+        if i%3==1:
+            result.append(rear(original,parse(line)))
+        i+=1
+    
+print(result)
+        
