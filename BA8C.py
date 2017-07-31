692926fa058b9cbf3bd722311d564717310731bf
Simon Crase <simon@greenweaves.nz>
2017-07-31 17:45:23 +1200

    BA8C solved

--- BA8C.py
+++ BA8C.py
@@ -6,7 +6,7 @@ def distance(p1,p2,m):
 
 def kmeans(k,m,data,centres=trivial_centres):
     centroids=centres(k,m,data)
-    print (centroids)
+    #print (centroids)
     step_size=float("inf")
     while step_size>0:
         new_centroids=step(k,m,data,centroids)
@@ -26,7 +26,7 @@ def step(k,m,data,centres):
         return index
     
     def centroid(i,indices):
-        print ('centroid', i, indices)
+        #print ('centroid', i, indices)
         count=0
         point=[0 for j in range(m)]
         for j in range(len(data)):
@@ -44,8 +44,9 @@ if __name__=='__main__':
     m = -1
     k = -1
     points=[]
-    
-    with open('BA8C.txt') as f:
+ 
+    with open (r'C:\Users\Weka\Downloads\rosalind_ba8c.txt') as f:   
+    #with open('BA8C.txt') as f:
         for line in f:
             if k==-1:
                 values=line.strip().split()
@@ -53,4 +54,5 @@ if __name__=='__main__':
                 m=int(values[1])
             else:
                 points.append([float(v) for v in line.strip().split()])
-        print (kmeans(k,m,points))
+        for pt in kmeans(k,m,points):
+            print (' '.join('{0:.3f}'.format(p) for p in pt))
