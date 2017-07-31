4db3293f6acada63bc7794851985a05345e79fec
Simon Crase <simon@greenweaves.nz>
2017-08-01 08:39:37 +1200

    BA8D Implement the Soft k-Means Clustering Algorithm (hidden matrix)

--- BA8D.py
+++ BA8D.py
@@ -1,10 +1,26 @@
-def step(centres,points):
-    return centres
+import numpy as np
 
-def soft_kmeans(k,m,beta,points,steps=100):
+def soft_kmeans(k,m,beta,points,steps=1):
+    def distance(p1,p2):
+        return np.sqrt(sum((p1[i]-p2[i])**2 for i in range (m)))    
+    @np.vectorize
+    def hidden_matrix(i,j):
+        return np.exp(-beta*distance(centres[i],points[j]))
+    def step(centres):
+        ks=np.array(range(k))
+        ms=np.array(range(m))
+        kk,mm=np.meshgrid(ks,ms)
+        numerator=hidden_matrix(kk,mm)
+        print (numerator)
+        denominator=np.sum(numerator,axis=0)
+        print (denominator)
+        matrix=np.divide(numerator,denominator)
+        print (matrix)
+        return centres
+
     centres=points[:k]
     for i in range(steps):
-        centres=step(centres,points) 
+        centres=step(centres) 
     return centres
 
 if __name__=='__main__':
