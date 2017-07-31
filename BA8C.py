d27611c2238fcf9672e2e90807e3fe66c8c65c31
Simon Crase <simon@greenweaves.nz>
2017-07-31 17:26:41 +1200

    BA8C Coded up: under test

---
+++ BA8C.py
@@ -0,0 +1,54 @@
+def trivial_centres(k,m,data):
+    return [data[i] for i in range(k)]
+
+def distance(p1,p2,m):
+    return sum((p1[i]-p2[i])**2 for i in range (m))
+
+def kmeans(k,m,data,centres=trivial_centres):
+    centroids=centres(k,m,data)
+    step_size=float("inf")
+    while step_size>0:
+        new_centroids=step(k,m,data,centroids)
+        step_size=0
+        for i in range(k):
+            step_size+=distance(new_centroids[i],centroids[i],m)
+        centroids=new_centroids
+    return centroids
+
+def step(k,m,data,centres):
+    def nearest(p):
+        index=-1
+        best=float('inf')
+        for i in range(k):
+            d1=distance(p,centres[i],m)
+            if d1<best:
+                index=i
+                best=d1
+    def centroid(i,indices):
+        count=0
+        point=[0 for j in range(m)]
+        for j in range(len(data)):
+            if indices[j]==i:
+                count+=1
+            for l in range(m):
+                point[l]+=data[j][l]
+        return [p/count for p in point]
+    
+    indices = [nearest(p) for p in data]
+    
+    return [centroid(i,indices) for i in range(k)]
+
+if __name__=='__main__':
+    m = -1
+    k = -1
+    points=[]
+    
+    with open('BA8C.txt') as f:
+        for line in f:
+            if k==-1:
+                values=line.strip().split()
+                k=int(values[0])
+                m=int(values[1])
+            else:
+                points.append([float(v) for v in line.strip().split()])
+        print (kmeans(k,m,points))
