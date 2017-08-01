f28fbbc024cc3ff910667757621bda89d8bbb140
Simon Crase <simon@greenweaves.nz>
2017-08-01 14:46:57 +1200

    BA8D Implement the Soft k-Means Clustering Algorithm (passed open test)

--- BA8D.py
+++ BA8D.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def soft_kmeans(k,m,beta,points,steps=1):
+def soft_kmeans(k,m,beta,points,N=1):
     def distance(p1,p2):
         return np.sqrt(sum((p1[i]-p2[i])**2 for i in range (m)))    
     @np.vectorize
@@ -8,18 +8,21 @@ def soft_kmeans(k,m,beta,points,steps=1):
         return np.exp(-beta*distance(centres[i],points[j]))
     def step(centres):
         ks=np.array(range(k))
-        ms=np.array(range(m))
-        kk,mm=np.meshgrid(ks,ms)
-        numerator=hidden_matrix(kk,mm)
-        print (numerator)
+        ns=np.array(range((len(points))))
+        kk,mm=np.meshgrid(ns,ks,indexing='xy')
+        numerator=hidden_matrix(mm,kk)
         denominator=np.sum(numerator,axis=0)
-        print (denominator)
         matrix=np.divide(numerator,denominator)
-        print (matrix)
-        return centres
+        #print (matrix)
+        new_centres=[[] for i in range(k)]
+        for i in range(k):
+            for j in range(m):
+                x_i_j=sum(matrix[i,l]*points[l][j] for l in range(len(points)))/sum(matrix[i,l] for l in range(len(points)))
+                new_centres[i].append(x_i_j)
+        return new_centres
 
     centres=points[:k]
-    for i in range(steps):
+    for i in range(N):
         centres=step(centres) 
     return centres
 
@@ -40,5 +43,5 @@ if __name__=='__main__':
                 beta=float(line.strip())
             else:
                 points.append([float(v) for v in line.strip().split()])
-        for pt in soft_kmeans(k,m,beta,points):
+        for pt in soft_kmeans(k,m,beta,points,N=100):
             print (' '.join('{0:.3f}'.format(p) for p in pt))
