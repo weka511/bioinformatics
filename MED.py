624f6257dfbe95cbbf198884b1ff28d213bd3532
Simon Crase <simon@greenweaves.nz>
2017-09-09 16:58:48 +1200

    Solved MED

--- MED.py
+++ MED.py
@@ -1,31 +1,34 @@
-def med(n,A,k):
-    def helper(n,A):
-        a_min=A[0]
-        index=0
-        for kk in range(n):
-            if A[kk]<a_min:
-                index=kk
-                a_min=A[index]
-        if index==0:
-            return (a_min,index,A[1:])
-        if index==n-1:
-            return (a_min,index,A[0:-1])
-        else:
-            return (a_min,index,A[0:index]+A[index+1:])
-    AA=A[:]    
-    for kk in range(k):
-            a,_,AA=helper(len(AA),AA)
-            #if kk%100==0:
-                #print (kk,a)
-       
-    return a
 
-#print (med(11,[2, 36, 5, 21, 8, 13, 11, 20, 5, 4 ,1],8))
+import random
+
+def selection(A,k):
+    def split(v):
+        prefix=[]
+        suffix=[]
+        mid=[]
+        for a in A:
+            if a<v:
+                prefix.append(a)
+            elif a==v:
+                mid.append(a)
+            else:
+                suffix.append(a)
+        return (prefix,mid,suffix)
+    v=random.choice(A)
+    (prefix,mid,suffix)=split(v)
+    if k<=len(prefix):
+        return selection(prefix,k)
+    elif len(prefix)<k and k<=len(prefix)+len(mid):
+        return v
+    else:
+        return selection(suffix, k-len(prefix)-len(mid))
+    
+#print (selection([2, 36, 5, 21, 8, 13, 11, 20, 5, 4 ,1],8))
 
 if __name__=='__main__':
     import timeit
     start_time = timeit.default_timer()
-    with open('c:/Users/Weka/Downloads/rosalind_med.txt') as f:
+    with open('c:/Users/Weka/Downloads/rosalind_med(1).txt') as f:
         n=0
         A=[]
         k=0
@@ -40,5 +43,5 @@ if __name__=='__main__':
                 k=int(text)
             i+=1
         print('n={0},k={1}'.format(n,k))
-        print (med(n,A,k))
+        print (selection(A,k))
         print ('Elapsed: {0} seconds'.format(timeit.default_timer() - start_time))
