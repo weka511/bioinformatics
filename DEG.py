00013e317fbe3fccb9a0007b6451a74f7e9c4167
Simon Crase <simon@greenweaves.nz>
2017-09-12 09:27:11 +1200

    Added copyright notice

--- DEG.py
+++ DEG.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2017 Greenweaves Software Pty Ltd
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
 def deg(n,m,A):
     degrees=[0 for a in range(n)]
     for (a,b) in A:
@@ -20,24 +35,3 @@ if __name__=='__main__':
         #print(A[1:])
         print (deg(n,m,A[1:]))
         print ('Elapsed: {0} seconds'.format(timeit.default_timer() - start_time))
-#A=[
-   #[1, 2],
-   #[2, 3],
-   #[6, 3],
-   #[5, 6],
-   #[2, 5],
-   #[2, 4],
-   #[4, 1]] 
-
-#print (deg(6,7,A))
-
-#6 7
-#1 2
-#2 3
-#6 3
-#5 6
-#2 5
-#2 4
-#4 1
-
-#2 4 2 2 2 2
--- PAR.py
+++ PAR.py
@@ -1,3 +1,18 @@
+# Copyright (C) 2017 Greenweaves Software Pty Ltd
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
 def par(n,A):
     prefix=[]
     suffix=[A[0]]
@@ -8,7 +23,6 @@ def par(n,A):
             suffix.append(a)
     return prefix + suffix
 
-#print(par(9,[7, 2 ,5 ,6, 1, 3, 9, 4, 8]))
 
 if __name__=='__main__':
 
