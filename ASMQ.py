101e26cb4527b272bebe66d91dbf2f48b339dfd6
Simon Crase <simon@greenweaves.nz>
2017-09-14 15:31:50 +1200

    Started ASMQ

---
+++ ASMQ.py
@@ -0,0 +1,28 @@
+'''
+ ASMQ Assessing Assembly Quality with N50 and N75
+
+ Copyright (C) 2017 Greenweaves Software Pty Ltd
+
+ This is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This software is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
+'''
+
+def generate_contigs(S):
+    pass
+
+def asmq(S):
+    pass
+
+if __name__=='__main__':
+    print (asmq(['GATTACA','TACTACTAC','ATTGAT','GAAGA']))
