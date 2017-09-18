b48546d4ff394d75413b7aa70c7fa773605ac8cb
Simon Crase <simon@greenweaves.nz>
2017-09-19 08:57:20 +1200

    Added skeleton for BA4M (turnpike problem)

---
+++ BA4M.py
@@ -0,0 +1,24 @@
+'''
+    #BA4M 	Solve the Turnpike Problem 
+
+    Copyright (C) 2017 Greenweaves Software Pty Ltd
+
+    This is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This software is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+'''
+
+def BA4M(L):
+    pass
+
+if __name__=='__main__':
+    print ( BA4M([-10,-8,-7,-6,-5,-4,-3,-3,-2,-2,0,0,0,0,0,2,2,3,3,4,5,6,7,8,10]))
