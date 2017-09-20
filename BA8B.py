5c641ff6bee849ace03f8de106860977c25b97f9
Simon Crase <simon@greenweaves.nz>
2017-09-20 12:33:31 +1200

    Solved BA8B Compute the Squared Error Distortion

--- BA8B.py
+++ BA8B.py
@@ -13,25 +13,33 @@
 # You should have received a copy of the GNU General Public License
 # along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
+# BA8B Compute the Squared Error Distortion
+
 import random,math
 
-def SquaredErrorDistortion(data,k,m):
-   pass
+def SquaredErrorDistortion(centres,data,k,m):
+   def distance(p1,p2):
+      return sum([(p1[i]-p2[i])**2 for i in range(m)])
+ 
+   
+   def distance_to_nearest(pt):
+      return min([distance(pt,c) for c in centres])
+   
+   return sum([distance_to_nearest(pt) for pt in data])/len(data)
+
 
 if __name__=='__main__':
    m = -1
    k = -1
    points=[]
 
-#   with open (r'C:\Users\Weka\Downloads\rosalind_ba8a(1).txt') as f:   
-   with open (r'ba8a.txt') as f:   
+   with open (r'C:\Users\Weka\Downloads\rosalind_ba8b(1).txt') as f:   
       for line in f:
          if k==-1:
                values=line.strip().split()
                k=int(values[0])
                m=int(values[1])
          else:
+            if line[0:2]!='--':
                points.append([float(v) for v in line.strip().split()])
-
-   for pt in FarthestFirstTraversal(points,k,m):
-      print (' '.join('{0:.3f}'.format(p) for p in pt))
+   print( SquaredErrorDistortion(points[0:k],points[k:],k,m))
