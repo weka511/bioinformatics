507b1c86d1159620b671d31c2cc9d4f62922096a
Simon Crase <simon@greenweaves.nz>
2017-09-27 10:08:09 +1300

    Created skeltons for BA7E, BA7F, and BA7G

---
+++ BA7E.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2017 Greenweaves Software Pty Ltd
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
+# BA7E Implement the Neighbor Joining Algorithm 
+
+def NeighborJoining(D,n):
+    pass
+
+if __name__=='__main__':
+    N=4
+    D=[[0 ,  23  ,27  ,20],
+    [23,  0 ,  30 , 28],
+    [27 , 30,  0  , 30],
+    [20 , 28 , 30 , 0 ]]
+    
