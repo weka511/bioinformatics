05d0e9425688496fd0fca961b9966e3ff381f012
Simon Crase <simon@greenweaves.nz>
2019-02-05 14:49:21 +1300

    Calculate Catalan numbers correctly

--- cat.py
+++ cat.py
@@ -1,4 +1,4 @@
-#    Copyright (C) 2017 Greenweaves Software Pty Ltd
+#    Copyright (C) 2019 Greenweaves Software Limited
 #
 #    This is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
@@ -17,16 +17,36 @@
 
 from rosalind import RosalindException,verify_counts_complete_graph
 
-def CatalanNumbers(N):
-    cn=[1]
-    for n in range(0,N+1):
-        while len(cn)<n+1:
-            cn.append(sum([cn[k-1]*cn[n-k] for k in range(1,n+1)]))
-    return cn[-1]
+# 1 2 5 14
 
-def CountNonCrossingMatches(string):
-    counts=verify_counts_complete_graph(string)
+# Verified against list from http://mathforum.org/advanced/robertd/catalan.html
+#
+# 1 1
+# 2 2
+# 3 5
+# 4 14
+# 5 42
+# 6 132
+# 7 429
+# 8 1430
+# 9 4862
+# 10 16796
+# 11 58786
+# 12 208012
+# 13 742900
+# 14 2674440
+# 15 9694845
+
+def catalan(n):
+    c=[1]
+    for n0 in range(1,n+1):
+        c.append(sum([c[k]*c[n0-1-k] for k in range(n0)]))
+    return c[n]
+
+def cat(s):
+    return catalan(int(len(s)/2))
     
 if __name__=='__main__':
-    #print (CountNonCrossingMatches('AUAU'))
-    print (CatalanNumbers(12))
+#    print (cat('AUAU')%1000000)
+    for n in range(1,16):
+        print (n,catalan(n))
