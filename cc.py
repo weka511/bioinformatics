1e2d3a91201c89b50d5a88be176241fc62dee10a
Simon Crase <simon@greenweaves.nz>
2019-02-11 10:56:24 +1300

    Solved cc! Program is a bit hacky

--- cc.py
+++ cc.py
@@ -15,6 +15,8 @@
 #
 #    cc 	Connected Components
 
+# Create adjacency list, with both forward and backward links
+
 def create_adjacency(graph):
     m,n       = graph[0]
     product   = {}
@@ -31,7 +33,6 @@ def create_adjacency(graph):
     return m,n,product
 
 def explore(a,adjacency,explored,component):
-#    print ("Exploring",a)
     explored.add(a)
     component.append(a)
     for b in adjacency[a]:
@@ -50,34 +51,6 @@ def cc(graph):
             component = explore(a,adjacency,explored,[])
             for c in component:
                 components[c]=component
-# 826 [64, 88, 90, 142, 259, 306, 310, 362, 364, 826]
-# 64 [26, 40, 42, 46, 58, 59, 61, 64, 72, 90, 92, 98, 100, 106, 107, 111, 128, 131, 147, 153, 154, 155, 168, 169, 177, 180, 193, 203, 210, 214, 227, 232, 236, 240, 251, 257, 259, 279, 295, 304, 305, 306, 310, 321, 330, 337, 342, 350, 352, 368, 376, 382, 385, 389, 392, 400, 402, 414, 427, 434, 461, 472, 484, 502, 538, 544, 550, 580, 582, 586, 602, 626, 677, 697, 715, 774, 788, 797, 798, 802, 814, 816]
-
-    #while True:
-        #merges = []
-        #for k,vs in components.items():
-            #for v in vs:
-                #if v in backlinks:
-                    #for b in backlinks[v]:
-                        #if not b in vs:
-                            #merges.append((k, backlinks[v]))
-                            #break
- 
-        #print (len(merges))
-        #if len(merges)==0: break 
- 
-        #merged=[]
-        
-        #for k, backs in merges:
-            #if k in merged: continue
-            #merged.append(k)
-            #components[k] = sorted(list(set( components[k]+ backs)))
-            #for v in  components[k]:
-                #components[v]= components[k]
-                #merged.append(v)
-                
-        #if len(merges)==0: break
-
                     
     count = 0
     uniques = []
@@ -88,25 +61,27 @@ def cc(graph):
         else:
             for vv in v:
                 uniques.append(vv)
-            print(k,v)
             count+=1
     for d in duplicates:
         del components[d]
-   
+
+#   a few sanity checks
+
     nodes = sorted(list(set([v for k,vs in components.items() for v in vs])))
     assert len(nodes) ==m, '{0} not {1}'.format(len(nodes), m) 
     v0=0
     for v in nodes:
         assert v == v0+1
         v0=v
-    return count
+        
+    return count,components
 
 if __name__=='__main__':
     graph=[]
-    with open(r'C:\Users\Simon\Downloads\rosalind_cc(11).txt') as f:
+    with open(r'C:\Users\Simon\Downloads\rosalind_cc(12).txt') as f: #132
         for line in f:
             ll =line.strip().split()
             graph.append((int(ll[0]),int(ll[1])))
-        c=cc(graph)
-        print (c)
+        count,_ = cc(graph)
+        print (count)
  
