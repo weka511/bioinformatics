f2dc324da91f050f5500c7dab43579ab34c520f6
Simon Crase <simon@greenweaves.nz>
2019-02-15 11:23:22 +1300

    Simplified logic

--- lcsq.py
+++ lcsq.py
@@ -13,39 +13,28 @@
 #    You should have received a copy of the GNU General Public License
 #    along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
-
 # LCSQ 	Finding a Shared Spliced Motif 
 
 def lcsq(s,t):
-    def get_prefix(s,t):
-        prefix = []
-        for i in range(min(len(s),len(t))):
-            if s[i]==t[i]:
-                prefix.append(s[i])
-            else:
-                break
-        return prefix
-    
+    def get_longest(seqs):
+        best_so_far = []
+        for seq in seqs:
+            if len(seq)>len(best_so_far):
+                best_so_far = seq
+        return best_so_far
+   
     def lcsq_helper(s,t):
         if len(s)==0: return t
         if len(t)==0: return s
         seqs=[]
+        if (len(s)==1 or len(t)==1) and s[0]==t[0]:
+            seqs.append([s[0]])
         for m in range(1,len(s)):
-            for n in range(1,len(s)):
+            for n in range(1,len(t)):
                 seqs.append(lcsq_helper(s[0:m],t[0:n]) + lcsq_helper(s[m:],t[n:]))
-        lcsq=[]
-        for seq in seqs:
-            if len(seq)>len(lcsq):
-                lcsq=seq
-        return lcsq
+        return get_longest(seqs)
     
-    s1 = [s0 for s0 in s]
-    t1 = [t0 for t0 in t]
-    prefix = get_prefix(s1,t1)
-    suffix = list(reversed(get_prefix(list(reversed(s1)),
-                                      list(reversed(t1)))))
-    return ''.join(prefix + lcsq_helper(s1[len(prefix):len(s1)-len(suffix)],
-                                        t1[len(prefix):len(t1)-len(suffix)]) + suffix)
+    return ''.join(lcsq_helper([s0 for s0 in s], [t0 for t0 in t]))
     
 if __name__=='__main__':
     print (lcsq('AACCTTGG','ACACTGTGA'))
