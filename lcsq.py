e93329ae922a9196fb1c361cfda901f40293acbc
Simon Crase <simon@greenweaves.nz>
2019-02-15 16:07:00 +1300

    Coded up dynamic programming version - under test

--- lcsq.py
+++ lcsq.py
@@ -15,6 +15,34 @@
 
 # LCSQ 	Finding a Shared Spliced Motif 
 
+def lcsq_dp(s,t):
+    lcs=[[0 for j in t] for i in s]
+    def get_lcs(i,j):
+        return lcs[i][j] if i>=0 and j>=0 else 0
+    def populate():
+        for i in range(len(s)):
+            for j in range(len(t)):
+                lcs[i][j] = (get_lcs(i-1,j-1) + 1) if s[i]==t[j] else max(get_lcs(i-1,j), get_lcs(i,j-1))
+        for i in range(len(s)):
+            print (lcs[i])
+            
+    def extract():        
+        i = len(s)-1
+        j = len(t)-1
+        seq = []
+        while True:
+            if lcs[i][j-1] == lcs[i][j]: j -= 1
+            elif lcs[i-1][j] == lcs[i][j]: i -= 1
+            else:
+                assert s[i]==t[j]
+                seq.append(s[i])
+                i -= 1
+                j -= 1
+            if i<0 or j<0:
+                return seq[::-1]
+    populate()
+    return extract()
+
 def lcsq(s,t):
     def get_longest(seqs):
         best_so_far = []
@@ -34,8 +62,10 @@ def lcsq(s,t):
                 seqs.append(lcsq_helper(s[0:m],t[0:n]) + lcsq_helper(s[m:],t[n:]))
         return get_longest(seqs)
     
-    return ''.join(lcsq_helper([s0 for s0 in s], [t0 for t0 in t]))
+    return ''.join(lcsq_dp([s0 for s0 in s], [t0 for t0 in t]))
     
 if __name__=='__main__':
     print (lcsq('AACCTTGG','ACACTGTGA'))
+    
+    #print (lcsq_dp(['A','B','A','Z','D','C'],['B','A','C','B','A','D']))
 
