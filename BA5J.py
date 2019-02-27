e077ca16d1f7c1b4817ac0941171f6b7b97278c8
Simon Crase <simon@greenweaves.nz>
2019-02-28 11:19:15 +1300

    Rewrite using Stanford slides

--- BA5J.py
+++ BA5J.py
@@ -25,20 +25,32 @@ def san_kai(s,t, replace_score=blosum62,sigma=11,epsilon=1):
     lower  =  create_distance_matrix(len(s)+1,len(t)+1)
     middle =  create_distance_matrix(len(s)+1,len(t)+1)
     upper  =  create_distance_matrix(len(s)+1,len(t)+1)
-    for i in range(len(s)):
-        for j in range(len(t)):
-            if i==0 and j==0: next
-            lower[i][j]  = max(lower[i-1][j]-epsilon,
-                               middle[i-1][j]-sigma)
-            middle[i][j] = max(lower[i][j],
-                               middle[i-1][j-1] + score((s[i],t[i])),
-                               upper[i][j])
-            upper[i][j]  = max(upper[i][j-1] - epsilon,
-                               middle[i][j-1] - sigma)
+    for i in range(len(s)+1):
+        for j in range(len(t)+1):
+            #if i==0 and j==0:
+                #pass
+            #elif i==0:
+            if i==0:
+                lower[i][j] = - (sigma + epsilon *(j-1))
+                upper[i][j] = - (sigma + epsilon *(j-1)) 
+ 
+            elif j==0:
+                lower[i][j] = - (sigma + epsilon *(i-1))
+                upper[i][j] = - (sigma + epsilon *(i-1))
+
+            else:
+                lower[i][j]  = max(lower[i-1][j]    - epsilon,
+                                   middle[i-1][j]   - sigma)
+                middle[i][j] = max(lower[i][j],
+                                   middle[i-1][j-1] + score((s[i-1],t[i-1])) ,
+                                   upper[i][j])     
+                upper[i][j]  = max(upper[i][j-1]    - epsilon,
+                                   middle[i][j-1]   - sigma)
+        
     return middle[len(s)][len(t)],[],[]
 
 def ba5j(s,t):
-    score,s1,t1 = san_kai([s0 for s0 in s],[t1 for t1 in t])
+    score,s1,t1 = san_kai([s0 for s0 in s],[t0 for t0 in t])
     return score,''.join(s1),''.join(t1)
 
 if __name__=='__main__':
