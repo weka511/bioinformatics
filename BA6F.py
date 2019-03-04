54d1c1428e3194f2429c0bd8609ad0f3310532a3
Simon Crase <simon@greenweaves.nz>
2019-03-05 10:09:24 +1300

    Issue #10 - refactored common code

--- BA6F.py
+++ BA6F.py
@@ -15,17 +15,7 @@
 #
 # BA6F Implement Chromosome to Cycle
 
-def ChromosomeToCycle(Chromosome):
-    Nodes = []
-    for i in Chromosome:
-        if i> 0:
-            Nodes.append(2*i-1)
-            Nodes.append(2*i)
-        else:
-            Nodes.append(-2*i)
-            Nodes.append(-2*i-1)
-        
-    return Nodes
+from fragile import ChromosomeToCycle
 
 if __name__=='__main__':
     print (ChromosomeToCycle([-1, -2, -3, -4, +5, -6, +7, +8, +9, -10, -11, +12, -13, +14, -15, +16, +17, -18, -19, -20, -21, -22, +23, +24, -25, +26, -27, -28, -29, -30, -31, -32, -33, +34, +35, +36, -37, +38, +39, -40, +41, -42, -43, +44, +45, +46, -47, +48, -49, -50, +51, +52, -53, +54, +55, +56, -57, +58, -59, -60, -61, -62, +63, +64, +65, +66, +67, +68]))
