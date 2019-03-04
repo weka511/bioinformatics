76a54041e22b4e238346e461d9c4f7d359bc7b53
Simon Crase <simon@greenweaves.nz>
2019-03-05 10:09:48 +1300

    Issue #10 - started extracting cycles

--- BA6C.py
+++ BA6C.py
@@ -15,15 +15,29 @@
 #
 #    BA6C Compute the 2-Break Distance Between a Pair of Genomes
 
-def ba6c(a,b):
-    def blocks():
-        return 0
+from fragile import count_synteny_blocks,get_synteny_blocks
+
+def extract_all_edges(chromosome):
+    def extract_edges(graph):
+        return list(zip(graph,graph[1:]+graph[0:]))
+    return [extract_edges(graph) for graph in chromosome]
+    
+def d2break(a,b):
     def cycles():
         return 0
-    return blocks()-cycles()
+    blocks = get_synteny_blocks(a)
+    n      = count_synteny_blocks(a)
+    nb     = count_synteny_blocks(b)
+    assert (n==nb),'Mismatched synteny blocks {0} != {1}'.format(n,nb)
+    e1     = extract_all_edges(a)
+    e2     = extract_all_edges(b)
+    print (e1)
+    print (e2)
+    return n-cycles()
 
 if __name__=='__main__':
-    print (ba6c(
-        [[+1, +2, +3, +4, +5, +6]],
-        [[+1, -3, -6, -5],[+2, -4]]
+    print (
+        d2break(
+            [[+1, +2, +3, +4, +5, +6]],
+            [[+1, -3, -6, -5],[+2, -4]]
     ))
