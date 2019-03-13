6d960778350ab80199d42c36b54d52d88f0bb922
Simon Crase <simon@greenweaves.nz>
2019-03-14 12:29:59 +1300

    Fixed #20 - exploit Dijkstra's algorithm

--- cte.py
+++ cte.py
@@ -18,81 +18,39 @@
 # Returns: For each graph, output the length of a shortest cycle going
 #           through the first specified edge if there is a cycle and "-1" otherwise.
 
+from dij import dij
 
-def cte(edges,n=25):
-    def create_adj():
-        product = {}
-        for a,b,w in edges[1:]:
-            if not a in product:
-                product[a]=[]
-            product[a].append((b,w))
-        return product
-    adj = create_adj()        
+def cte(edges):
+    def relabel(edges):
+        def subst(x):
+            if   x==1: return b
+            elif x==b: return 1
+            else:
+                return x
+        def transform(x,y,w):
+            return (subst(x),subst(y),w)
 
-    def explore(start,node,path=[],length=0):
-        total_length = -1
-        if not node in adj: return -1
-        linked = adj[node]
-        for succ,weight in linked:
-            if succ==start:
-                return length+weight
-            if succ == node: continue
-            if succ in path: continue
-            total_length=explore(start,succ,path+[node],length+weight)
-            if total_length>0:
-                return total_length
-        return -1
 
-    explored   = set()
-   
-    a,b,w  = edges[1]
-    max_length = explore(start=a,node=b,path=[a],length=w)
-    print (max_length)
-            
-    #start,b,w          = edges[1]
-
-    #partial_cycles = [([start,b],w)]
-    #cycles         = []
+        return [(n,k-1)] + [transform (x,y,w) for (x,y,w) in edges[2:]]
     
-    #while len(partial_cycles)>0:
-        #partial_cycles.sort(key=lambda tup: len(tup[0]),reverse=True)
-        #ccc,_ = partial_cycles[0]
-        #print (len(ccc),len(partial_cycles))        
-        #new_cycles=[]
-        #for (cc,length) in partial_cycles[:n]:
-            #if cc[-1] in adj:
-                #for next_node,weight in adj[cc[-1]]:
-                    #if not next_node in cc[:-1]:
-                        #new_cycles.append((cc+[next_node],length+weight))
-        
-        ##ccc,_ = new_cycles[0]
-        ##print (len(ccc),len(new_cycles))
-        #leaders = []
-        #for (cc,length) in new_cycles:
-            ##print (cc[-1],cc[0])
-            #if cc[-1]==cc[0]:
-                #cycles.append((cc,length))
-            ##elif not cc[-1] in cc[:-1]:
-            #else:
-                #leaders.append((cc,length))
-        #for ccc in partial_cycles[n:]:
-            #leaders.append(ccc)
-        #partial_cycles = leaders
-            ##else:
-                ##print ("dropped",cc[-1],cc[:-1])
-    #return min([w for (_,w) in cycles]) if len(cycles)>0 else -1
-
+    n,k   = edges[0]
+    a,b,w = edges[1]        
+    new_edges = relabel(edges)
+    #print (new_edges)
+    path_length = dij(new_edges)[a-1]
+    return path_length + w if path_length>-1 else -1
 
 if __name__=='__main__':
     from helpers import create_strings
 
     #k = 2
-    #gs = [[(4, 5),
-           #(2, 4, 2),
-           #(3, 2, 1),
-           #(1, 4, 3),
-           #(2, 1, 10),
-           #(1, 3, 4)],
+    #gs = [
+          ##[(4, 5),
+           ##(2, 4, 2),
+           ##(3, 2, 1),
+           ##(1, 4, 3),
+           ##(2, 1, 10),
+           ##(1, 3, 4)],
 
           #[(4, 5),
            #(3, 2, 1),
@@ -103,7 +61,7 @@ if __name__=='__main__':
     #print (' '.join([str(cte(g)) for g in gs]))   
     gs = []
     g  = []
-    for line in create_strings():
+    for line in create_strings(ext=1):
         numbers = [int(s) for s in line.split(' ')]
         if len(numbers)==1:
             pass
