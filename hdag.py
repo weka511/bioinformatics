4034c0570dd2ca8f500ccbbc292587b472f0f690
Simon Crase <simon@greenweaves.nz>
2019-03-15 07:05:54 +1300

    Solved HDAG

--- hdag.py
+++ hdag.py
@@ -17,70 +17,45 @@
 #
 
 from helpers import create_adjacency
+from align import topological_order
 
-def hdag(graph):
-    def explore(start,path=[]):
-        if len(path)==m: return 1,path
-        if start in adjacency:
-            for next_node in adjacency[start]:
-                if next_node in path: continue
-                result,path = explore(next_node,path=path+[next_node])
-                if result==1 and len(path)==m: return result,path
-                if result==-1: return -1,[]
-            pass
-        else:
-            return -1,[]
-        return -1,[]
-    
-    m,_,adjacency = create_adjacency(graph,back=False,self=False)
-    
-    for start in range(1,m+1):
-        result,path = explore(start,path=[start])
-        if result>0:
-            return result,path
+def hdag(graph):  
+    m,_,adj = create_adjacency(graph,back=False,self=False)
+    t = topological_order(adj)
+    m,_,adj = create_adjacency(graph,back=False,self=False)
+    for a,b in zip(t[:-1],t[1:]):
+        if not b in adj[a]:
+            return (-1,[])
+    return (1,t)
         
-    return  result,path
 
 if __name__=='__main__':
     from helpers import create_strings
-    
-    k=2
-    
-    graphs = [[(3,3),
-               (1,2),
-               (2,3),
-               (1, 3)],
-    
-              [(4, 3),
-                (4,3),
-                (3, 2),
-                (4, 1)]]    
+       
+    gs = []
+    g  = []
+    for line in create_strings(ext=1):
+        if len(line)==0:
+            if len(g)>0:
+                gs.append(g)
+            g=[]
+            continue
+        numbers = [int(s) for s in line.split(' ')]
+        if len(numbers)==1:
+            continue
+        elif len(numbers)==2:
+            
+            g.append((numbers[0],numbers[1]))
 
-    for g in graphs:
+    if len(g)>0:
+        gs.append(g)           
+ 
+    
+    for g in gs:
         result,path=hdag(g)
         if result==-1:
             print (result)
         else:
             print (' '.join(str(x) for x in [result]+path) )
-    
-    #gs = []
-    #g  = []
-    #for line in create_strings(ext=1):
-        #if len(line)==0:
-            #if len(g)>0:
-                #gs.append(g)
-            #g=[]
-            #continue
-        #numbers = [int(s) for s in line.split(' ')]
-        #if len(numbers)==1:
-            #continue
-        #elif len(numbers)==2:
-            
-            #g.append((numbers[0],numbers[1]))
-
-    #if len(g)>0:
-        #gs.append(g)           
+                
  
-        
-    #print (' '.join([str(sq(g)) for g in gs]))    
-  
