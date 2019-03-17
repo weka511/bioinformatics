ed3084c971fb26d3606bf1cd9494815a624a7fa3
Simon Crase <simon@greenweaves.nz>
2019-03-17 16:32:24 +1300

    Issue #22: Passed file test case

--- scc.py
+++ scc.py
@@ -13,8 +13,32 @@
 #    You should have received a copy of the GNU General Public License
 #    along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
 
-#    NWC Negative Weight Cycle
+#    SCC Strongly Connected Component
 #
+
+def dfs(adj         = None,
+        n           = None,
+        sequence    = None,
+        previsit    = lambda v:None,
+        postvisit   = lambda v:None,
+        pre_explore = lambda v:None):
+
+  def explore(v):
+    visited[v] = True
+    previsit(v)
+
+    for u in adj[v]:
+      if not visited[u]:
+        explore(u)
+          
+    postvisit(v)   
+  
+  visited = [False for v in range(n+1)]  # Zero element won't be used, but it does simplify indexing
+
+  for v in sequence:
+    if not visited[v]:
+      pre_explore(v)
+      explore(v)
 
     
 def scc(edges):
@@ -26,83 +50,65 @@ def scc(edges):
       count+=1
     
   def create_adj(edges,reverse=False):
+    n,_=edges[0]
     adj = {}
+    for i in range(1,n+1):
+      adj[i]=[]
     for (a,b) in edges[1:]:
       if reverse:
         (a,b)=(b,a)
-      if not a in adj:
-        adj[a]=[]
       adj[a].append(b)
     return adj
-  
- 
-  def dfs(adj=None,
-          sequence=None,
-          previsit=lambda v:None,
-          postvisit=lambda v:None,
-          pre_explore=lambda v:None):
-
-    def explore(v):
-      visited[v] = True
-      previsit(v)
-
-      if v in adj:
-        for u in adj[v]:
-          if not visited[u]:
-            explore(u)
-            
-      postvisit(v)   
-      return
     
-    visited = [False for v in range(n+1)]  # Don't use zero
-
-    for v in sequence:
-      if not visited[v]:
-        pre_explore(v)
-        explore(v)
-    return visited,pre,post
-  
   def decreasing(post):
-    pairs = sorted(zip(post,range(len(post))))
+    pairs = sorted(zip(post,range(1,len(post)+1)),reverse=True)
     for a,b in pairs:
       yield b
 
   def incr_pre(v):
     pre[v]     = next(clock)
+    
   def incr_post(v):
     post[v]     = next(clock)
 
   def incr(v):
     ccnum[v-1]=next(cc)
     
-  n,_ = edges[0]
-  clock = counter()
-  pre     = [-1 for v in range(n+1)]
-  post    = [-1 for v in range(n+1)]  
+  n,_     = edges[0]
+  clock   = counter()
+  pre     = [-1 for v in range(n+1)]   # Zero element won't be used, but it does simplify indexing
+  post    = [-1 for v in range(n+1)]   # Zero element won't be used, but it does simplify indexing 
      
-  dfs(adj=create_adj(edges,reverse=True),
-      sequence=range(1,n+1),
-      previsit=incr_pre,
-      postvisit=incr_post)
+  dfs(adj      = create_adj(edges,reverse=True),
+      n        = n,
+      sequence = range(1,n+1),
+      previsit = incr_pre,
+      postvisit = incr_post)
   
-  cc = counter()
-  ccnum    = [-1 for v in range(n+1)]
+  cc       = counter()
+  ccnum    = [-1 for v in range(n+1)]   # Zero element won't be used, but it does simplify indexing
 
-  dfs(adj=create_adj(edges),
-      sequence=decreasing(post[1:]),
-      pre_explore=incr)
+  dfs(adj         = create_adj(edges),
+      n           = n,
+      sequence    = decreasing(post[1:]),
+      pre_explore = incr)
+  
   return len([cc for cc in ccnum if cc>-1])     
 
 if __name__=='__main__':
-  edges = [(6, 7),
-           (4, 1),
-           (1, 2),
-           (2, 4),
-           (5, 6),
-           (3, 2),
-           (5, 3),
-           (3, 5)
-           ]
-  print (scc(edges))
+  from helpers import create_list
+  #print (
+    #scc([
+      #(6, 7),
+      #(4, 1),
+      #(1, 2),
+      #(2, 4),
+      #(5, 6),
+      #(3, 2),
+      #(5, 3),
+      #(3, 5)
+  #]))
+  
+  print (scc(create_list(ext=4)))
                
 
