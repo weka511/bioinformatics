978f5a4b6331c4f38ab3d35c23beeff098855e56
Simon Crase <simon@greenweaves.nz>
2019-03-26 20:55:30 +1300

    Coded up skeleton

--- BA5M.py
+++ BA5M.py
@@ -17,10 +17,77 @@
 
 
 from Bio.SubsMat.MatrixInfo import blosum62
+from numpy import argmax
 
-
-def FindHighestScoringMultipleSequenceAlignment (v,w,x,replace_score=blosum62,indel_cost=5):
-    pass
+def FindHighestScoringMultipleSequenceAlignment (u,v,w,score=lambda x,y,z: 1 if x==y and y==z else 0):
+    s = [[[0 for i in range(len(w)+1)] for j in range(len(v)+1)] for k in range(len(u)+1) ]
+    moves = {}
+    for i in range(1,len(u)+1):
+        for j in range(1,len(v)+1):
+            for k in range(1,len(w)+1):
+                scores     = [
+                    s[i-1][j][k]     + score(u[i-1], '-',    '-'),
+                    s[i][j-1][k]     + score('-',    v[j-1], '-'),
+                    s[i][j][k-1]     + score('-',    '-',    w[k-1]),
+                    s[i][j-1][k-1]   + score('-',    v[j-1], w[k-1]),
+                    s[i-1][j][k-1]   + score(u[i-1], '-',   w[k-1]),
+                    s[i-1][j-1][k]   + score(u[i-1],v[j-1], '-'),
+                    s[i-1][j-1][k-1] + score(u[i-1],v[j-1],w[k-1])
+                ]
+                possible_moves = [
+                    (-1,  0,   0),
+                    (0,  -1,   0),
+                    (0,  0,   -1),
+                    (0,  -1,  -1),
+                    (-1, 0,  -1),
+                    (0,  -1, -1),
+                    (-1, -1, -1),
+                ]
+                index          = argmax(scores)
+                s[i][j][k]     =    scores[index]
+                moves[(i,j,k)] = possible_moves[index]
+    i  = len(u)
+    j  = len(v)
+    k  = len(w)
+    u1 = []
+    v1 = []
+    w1 = []
+    while i>0 and j>0 and k>0:
+        di,dj,dk = moves[(i,j,k)]
+        i+=di
+        j+=dj
+        k+=dk
+        if dj==0 and dk==0:
+            u1.append(u[i])
+            v1.append('-')
+            w1.append('-')
+        elif di==0 and dk==0:
+            u1.append('-')
+            v1.append(v[j])
+            w1.append('-')
+        elif di==0 and dj==0:
+            u1.append('-')
+            v1.append('-')
+            w1.append(w[k])
+        elif di==0:
+            u1.append('-')
+            v1.append(v[j])
+            w1.append(w[k])
+        elif dj==0:
+            u1.append(u[i])
+            v1.append('-')
+            w1.append(w[k])
+        elif dk==0:
+            u1.append(u[i])
+            v1.append(v[j])
+            w1.append('-')
+        else:
+            u1.append(u[i])
+            v1.append(v[j])
+            w1.append(w[k])        
+            
+    return s[len(u)][len(v)][len(w)],''.join(u1[::-1]),''.join(v1[::-1]),''.join(w1[::-1])             
+            
 
 if __name__=='__main__':
     from helpers import create_strings    
