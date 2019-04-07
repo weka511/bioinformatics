d93b3ebdc8a92f292bbbabf2e0b3a5b9d61c33e8
Simon Crase <simon@greenweaves.nz>
2019-04-07 14:11:52 +1200

    WIP checkin (after suspecding work)

--- BA9C.py
+++ BA9C.py
@@ -15,50 +15,115 @@
 
 # BA9C Construct the Suffix Tree of a String 
 
+# ConstructModifiedSuffixTrie
+#
+# Construct Modified Suffix Trie, as described in Charging Station
+# Bioinfornmatics Algorithms Voll II page 165
+
+def ConstructModifiedSuffixTrie(Text):
+    # edgeLabelled
+    #
+    # Find edge labelled with symbol
+    #
+    # Inputs: currentNode
+    #         currentSymbol
+    #
+    # Returns: node that edge links to, or None
+    
+    def edgeLabelled(currentNode, currentSymbol):
+        if currentNode in Trie:
+            for symbol,_,node in Trie[currentNode]:
+                if symbol==currentSymbol:
+                    return node
+        return None
+    
+    ROOT            = 0    
+    Trie            = {ROOT:[]}
+    labels          = {}
+    next_avail_node = 1
+    for i in range(len(Text)):
+        currentNode = ROOT
+        for j in range(i,len(Text)):
+            currentSymbol = Text[j]
+            end_node_labelled_edge = edgeLabelled(currentNode, currentSymbol)
+            if end_node_labelled_edge != None:
+                currentNode = end_node_labelled_edge
+            else:
+                Trie[next_avail_node] = []
+                Trie[currentNode].append((currentSymbol,j,next_avail_node))
+                currentNode = next_avail_node
+                next_avail_node+=1
+        if len(Trie[currentNode])==0:
+            labels[currentNode] = i
+    return Trie,labels
+
 # ConstructSuffixTree
 #
-# Construct the suffix tree of a string.
+# Construct the suffix tree of a string, as described in Charging Station
+# Bioinfornmatics Algorithms Voll II page 167
 #
 # Inputs: A string Text.
 #
 # Return: The strings labeling the edges of SuffixTree(Text).
 
-from rosalind import trie
-
-def ConstructModifiedSuffixTrie(Text):
-    def edgeLabelled(currentNode, currentSymbol):
-        if currentNode in Trie:
-            for symbol,pos,node in Trie[currentNode]:
-                if symbol==currentSymbol:
-                    return node
-        return None
-        
-    Trie   = {0:[]}
-    labels = {}
-    n      = 1
-    for i in range(len(Text)):
-        currentNode = 0
-        for j in range(i,len(Text)):
-            currentSymbol = Text[j]
-            next_node = edgeLabelled(currentNode, currentSymbol)
-            if next_node != None:
-                currentNode = next_node
-            else:
-                Trie[n] = []
-                Trie[currentNode].append((currentSymbol,j,n))
-                currentNode = n
-                n+=1
-        if len(Trie[currentNode])==0:
-            labels[currentNode] = i
-    return Trie,labels
-
 def ConstructSuffixTree(Text):
+    # dfs
+    #
+    # Search Trie for non-branching paths
+    #
+    # Inputs: currentNode
+    #         path
+    def dfs(currentNode,path=[]):
+        if len(Trie[currentNode])<2: # Non-branching or end
+            path = path + [currentNode]
+            if len(Trie[currentNode])==0: #end of branch
+                symbols=[]
+                for step in path:
+                    if (len(Trie[step]))>0:
+                        symbol,_,_=Trie[step][0]
+                        symbols.append(symbol)
+                    #else:
+                        #symbols.append(str(labels[step]))
+                    
+                return ''.join(symbols)
+        else:
+            path=[]
+        for symbol,pos,next_node in Trie[currentNode]:
+            symbols=dfs(next_node,path)
+            if symbols!=None:
+                print (symbols)
+                Trie[currentNode]=[(symbols,pos,next_node)]   
+            
     Trie,labels = ConstructModifiedSuffixTrie(Text)
+    Paths=[]
+    dfs(0)
     return Trie
 
-if __name__=='__main__':
-    print (ConstructSuffixTree('ATAAATG$'))
+# PrintTrie
+#
+# Print Trie
+
+def PrintTrie(Trie):
     
-    trie,labels = ConstructModifiedSuffixTrie('panamabananas$')
-    print (trie)
-    print (labels)
+    # dfs
+    #
+    # Depth First Search, printing nodes as we go
+    
+    def dfs(Trie,currentNode=0,depth=1):
+        for symbol,pos,next_node in Trie[currentNode]:
+            padding = ''.join(['-']*depth)
+            print ('{0} {1}, {2}, {3}'.format(padding,symbol,pos,next_node))
+            dfs(Trie,next_node,depth+1)
+    print ('root')
+    dfs(Trie)
+
+if __name__=='__main__':
+    #for k,v in ConstructSuffixTree('ATAAATG$').items():
+        #print (k,v)    
+    Trie,labels=ConstructModifiedSuffixTrie('panamabananas$')
+    PrintTrie(Trie)
+    Trie1 = ConstructSuffixTree('panamabananas$')
+    PrintTrie(Trie1)
+    #trie,labels = ConstructModifiedSuffixTrie('panamabananas$')
+    #print (trie)
+    #print (labels)
