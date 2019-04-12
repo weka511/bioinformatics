c7ab312a8fa877cca6265ca563451bf1d45c28c6
Simon Crase <simon@greenweaves.nz>
2019-04-12 16:47:58 +1200

    Issue #13 BA6D Find a Shortest Transformation of One Genome into Another by 2-Breaks

---
+++ BA6d.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2019 Greenweaves Software Limited
+
+# This is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+
+# BA6D Find a Shortest Transformation of One Genome into Another by 2-Breaks 
+
+
+
+if __name__=='__main__':
+    pass
