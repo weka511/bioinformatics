4a921089c4571922057aee13806c5ac62830d345
Simon Crase <simon@greenweaves.nz>
2019-05-26 10:51:04 +1200

    Additional doco

--- spectrum.py
+++ spectrum.py
@@ -127,8 +127,8 @@ def create_extended():
 
 def CreatePeptideVector(peptide):
     extended_masses = create_extended()
-    masses = [extended_masses[p] for p in peptide]
-    result = []
+    masses          = [extended_masses[p] for p in peptide]
+    result          = []
     for m in masses:
         result = result + ([0]*(m-1))
         result.append(1)
@@ -148,23 +148,44 @@ def CreatePeptide(vector):
 # conv
 #
 # Comparing Spectra with the Spectral Convolution
+#
+# Comparing Spectraclick to collapse
+#
+# Suppose you have two mass spectra, and you want to check if they both were obtained from the same protein; 
+# you will need some notion of spectra similarity. The simplest possible metric would be to count the number
+# of peaks in the mass spectrum that the spectra share, called the shared peaks count; 
+# its analogue for simplified spectra is the number of masses that the two spectra have in common.
+
+# The shared peaks count can be useful in the simplest cases, but it does not help us if, for example,
+# one spectrum corresponds to a peptide contained inside of another peptide from which the second
+# spectrum was obtained. In this case, the two spectra are very similar, but the shared peaks count
+# will be very small. However, if we shift one spectrum to the right or left, then shared peaks will align.
+# In the case of simplified spectra, this means that there is some shift value `x` such that adding
+# x to the weight of every element in one spectrum should create a large number of matches in the other spectrum.
+#
+# Inputs: Two multisets of positive real numbers S1 and S2
+#
+# The size of each multiset is at most 200.
+
+# Return: The largest multiplicity of S1- S2, as well as the absolute value of the number x 
+# maximizing (S1-S2)(x) (you may return any such value if multiple solutions exist).
 
 def conv(S,T,eps=0.001):
-    diff = sorted([s-t for s in S for t in T])
-    accumulated = []
-    latest      = diff[0]
-    count       = 1
+    minkowski_diff = sorted([s-t for s in S for t in T])
+    accumulated    = []
+    latest         = minkowski_diff[0]
+    count          = 1
     
-    for term in diff[1:]+[666]:
+    for term in minkowski_diff[1:]+[666]:
         if abs(term-latest)<eps:
             count+=1
         else:
             accumulated.append((count,latest))
             latest = term
             count  = 1
-    index_max = argmax([i for i,_ in accumulated])
-    c,v = accumulated[index_max]
-    return c,v
+            
+    return accumulated[argmax([i for i,_ in accumulated])]
+    
 
 def create_lookup(amino_acids=amino_acids):
     pairs = sorted([(abbrev,value.mon_mass) for abbrev,value in amino_acids.items()],
@@ -186,6 +207,39 @@ def get_abbrev(diff,masses,pairs):
 # spectrum2protein
 #
 # spec Inferring Protein from Spectrum
+#
+#    Introduction to Mass Spectrometry
+#
+#    In “Calculating Protein Mass”, we briefly mentioned an analytic chemical method called mass spectrometry,
+#    which aims to measure the mass-to-charge ratio of a particle or a molecule. In a mass spectrometer,
+#    a sample is vaporized (turned into gas), and then particles from the sample are ionized. The resulting
+#    ions are placed into an electromagnetic field, which separates them based on their charge and mass.
+#    The output of the mass spectrometer is a mass spectrum, or a plot of ions' possible mass-to-charge ratio 
+#    values with the intensity (actual observed frequency) of ions having these mass-to-charge values.
+#
+#    For the moment, we will ignore charge and consider a list of the ions' monoisotopic masses as a
+#    simplified spectrum. Researchers do not possess cheap technology to go in and examine a protein one
+#    amino acid at a time (molecules are too submicroscopic). Instead, to determine a protein's structure,
+#    we will split several copies of the protein into smaller pieces, then weigh the resulting fragments. 
+#    To do this, we assume that each cut (breakage point) occurs between two amino acids and that we can 
+#    measure the mass of the resulting pieces for all possible cuts.
+#
+#    For example, the (unknown) protein "PRTEIN" can be cut in five possible ways:
+#    "P" and "RTEIN"; "PR" and "TEIN"; "PRT" and "EIN"; "PRTE" and "IN"; "PRTEI" and "N".
+#    We then can measure the masses of all fragments, including the entire string. The "left" end of
+#    a protein is called its N-terminus, and the ions corresponding to the protein string's prefixes 
+#    (P, PR, PRT, PRTE, PRTEI) are called b-ions. The "right" end of the protein is called its C-terminus,
+#    and the ions corresponding to the string's suffixes (N, IN, EIN, TEIN, RTEIN) are called y-ions. 
+#    The difference in the masses of two adjacent b-ions (or y-ions) gives the mass of one amino acid residue; 
+#    for example, the difference between the masses of "PRT" and "PR" must be the mass of "T." By extension,
+#    knowing the masses of every b-ion of a protein allows us to deduce the protein's identity.
+#
+# The prefix spectrum of a weighted string is the collection of all its prefix weights.
+#
+# Input: A list L of n (n<=100) positive real numbers.
+#
+# Return: A protein string of length n-1 whose prefix spectrum is equal to L (if multiple solutions exist,
+# you may output any one of them). Consult the monoisotopic mass table.
 
 def spectrum2protein(ms):
     masses,pairs = create_lookup() 
@@ -204,6 +258,21 @@ def complete_spectrum(P):
 #
 # Match a Spectrum to a Protein
 #
+# Searching the Protein Database
+#
+# Many proteins have already been identified for a wide variety of organisms. Accordingly,
+# there are a large number of protein databases available, and so the first step after 
+# creating a mass spectrum for an unidentified protein is to search through these databases
+# for a known protein with a highly similar spectrum. In this manner, many similar proteins 
+# found in different species have been identified, which aids researchers in determining protein function.
+#
+# In “Comparing Spectra with the Spectral Convolution”, we introduced the spectral convolution 
+# and used it to measure the similarity of simplified spectra. In this problem, we would like
+# to extend this idea to find the most similar protein in a database to a spectrum taken from
+# an unknown protein. Our plan is to use the spectral convolution to find the largest possible
+# number of masses that each database protein shares with our candidate protein after shifting,
+# and then select the database protein having the largest such number of shared masses.
+
 # Inputs: A positive integer n followed by a collection of n protein strings s1, s2, ..., sn and a multiset R
 # of positive numbers (corresponding to the complete spectrum of some unknown protein string).
 
