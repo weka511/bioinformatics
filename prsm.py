a9aee6985a4741ef7d7f55e88198d9cf3b3c834d
Simon Crase <simon@greenweaves.nz>
2019-05-25 16:35:03 +1200

    Testing prsm.py

---
+++ prsm.py
@@ -0,0 +1,56 @@
+#    Copyright (C) 2019 Greenweaves Software Limited
+#
+#    This is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This software is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
+#
+#    prsm Matching a Spectrum to a Protein
+
+from spectrum import conv
+from reference_tables import amino_acids
+from numpy import argmax
+
+def complete_spectrum(P):
+    def spectrum(S):
+        return [amino_acids[s].mon_mass for s in S]
+    ss= [spectrum(P[i:j]) for i in range(len(P)) for j in range(i+1,len(P)+1)]
+    return [item for sublist in ss for item in sublist]
+
+def prsm(s,R):
+    def count(c):
+        return c[1][0]
+    m = 0
+    i = 0
+    Ss = [(P,complete_spectrum(P)) for P in s]
+    for p,ss in Ss:
+        print (p,ss)
+    Cs = [(P,conv(R,S1)) for (P,S1) in Ss]
+    for (p,(a,b)) in Cs:
+        print (p,a,b)
+    ii = argmax([count(c) for c in Cs])
+    print (ii,Cs[ii])
+    return m,s[i]
+
+if __name__=='__main__':
+    m,s_max = prsm(['GSDMQS',
+                    'VWICN',
+                    'IASWMQS',
+                    'PVSMGAD'],
+                   [445.17838,
+                    115.02694,
+                    186.07931,
+                    314.13789,
+                    317.1198,
+                    215.09061]
+                   )
+    print (m)
+    print (s_max)
