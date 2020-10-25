6ba9df8f54b895f6dd3925679e608ad825535fc5
Simon Crase <simon@greenweaves.nz>
2020-10-25 20:39:21 +1300

    #38: implemented dynamic programming - testing

--- sims.py
+++ sims.py
@@ -16,11 +16,48 @@
 # SIMS Finding Mutated Motifs
 
 import numpy as np
-def sims(s,t):
-    scores = 0
-    return (0,'','')
+
+def get_score(c1,c2, match=1,mismatch=-1):
+    return match if c1==c2 else mismatch
+
+def score_string(s,t):
+    return sum(get_score(c1,c2) for (c1,c2) in zip(s,t))
+
+def sims(s,t, match=1,mismatch=-1):
+    scores    = np.zeros((len(t),len(s)),dtype=int)
+    link_back = {}
+    for j in range(len(s)):
+        scores[0,j]  = get_score(s[j],t[0],match=match,mismatch=mismatch)
+    for i in range(1,len(t)):
+        for j in range(i,len(s)):
+            score_diag       = scores[i-1][j-1] + get_score(s[j],t[i],match=match,mismatch=mismatch)
+            score_horizontal = scores[i-1][j] + mismatch
+            if score_horizontal>score_diag:
+                scores[i,j] = score_horizontal
+                link_back[(i,j)] = (i,j-1)
+            else:
+                scores[i,j] = score_diag
+                link_back[(i,j)] = (i-1,j-1)
+    for i in range(len(scores)):
+        print (scores[i])
+    print (max(scores[-1]))
+    j0      = np.argmax(scores[-1])
+    i0      = len(scores)-1
+    s_match = [s[j0]]
+    t_match = [t[i0]]
+    while (i0,j0) in link_back:
+        i1,j1=link_back[(i0,j0)]
+        if i0==i1:
+            t_match.append('-')
+        else:
+            t_match.append(t[i1])
+        s_match.append(s[j1])
+        i0,j0= i1,j1
+    return (max(scores[-1]),''.join(s_match[::-1]),''.join(t_match[::-1]))
 
 if __name__=='__main__':
+    print(score_string('ACCATAAGCCCTACGTG-CCG',
+                'GCCGTCAGGC-TG-GTGTCCG'))
     
     print (
         sims('GCAAACCATAAGCCCTACGTGCCGCCTGTTTAAACTCGCGAACTGAATCTTCTGCTTCACGGTGAAAGTACCACAATGGTATCACACCCCAAGGAAAC',
