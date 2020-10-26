cfa9d9d3751c14bf8e447b6c3c92c32d6968f092
Simon Crase <simon@greenweaves.nz>
2020-10-27 11:01:50 +1300

    #40 Read data for extra dataset (not passing)

--- BA9C.py
+++ BA9C.py
@@ -16,6 +16,7 @@
 # BA9C Construct the Suffix Tree of a String 
 
 import argparse
+from helpers import read_strings
 from snp import ConstructSuffixTreeEdges    
 
 def check(Edges,Expected):
@@ -25,10 +26,28 @@ def check(Edges,Expected):
             mismatches+=1
             print (f'Expected {b}, was {a}')
     print(f'{0} mismatches')
-    
+
+def compare_edges(Edges,Expected):
+    print ('Expected = {len(Expected)} edges, actual={len(Edges)}')
+    expected = iter(sorted(Expected))
+    edges    = iter(sorted(Edges))
+    exp      = next(expected)
+    ed       = next(edges)
+    while exp != '-' and ed !='-':
+        if exp<ed:
+            print('{0},{1}'.format(exp,'-'))
+            exp = next(expected,'-')           
+        elif ed<exp:
+            print('{0},{1}'.format('-',ed))
+            ed = next(edges,'-') 
+        else:
+            exp = next(expected,'-')
+            ed = next(edges,'-')
+            
 if __name__=='__main__':
     parser = argparse.ArgumentParser('BA9C Construct the Suffix Tree of a String ')
     parser.add_argument('--sample', default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--extra',  default=False, action='store_true', help='process extra dataset')
     args = parser.parse_args()
     if args.sample:
         Edges = ConstructSuffixTreeEdges('ATAAATG$')
@@ -51,174 +70,12 @@ if __name__=='__main__':
             '$'        
         ]
         check(Edges,Expected)
-    else:
-        Edges = ConstructSuffixTreeEdges('ATCTACCAGCAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$')   
-      
-        Expected = [
-            '$',
-            'ATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'GCCGCCGCTGG$',
-            'C',
-            'AAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'GG',
-            'GACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'CAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'AG',
-            'CTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'C',
-            'GCCGCCGCTGG$',
-            'TCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'C',
-            'ACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'AGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'CTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'GTGTATAACGCCGCCGCTGG$',
-            'G',
-            'AAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'GAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'G',
-            'T',
-            'AACGCCGCCGCTGG$',
-            'CTACCAGCAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'GGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'T',
-            'G',
-            'ACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'AAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'GAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'G',
-            'T',
-            'TGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'CAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'AG',
-            'CTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CGCTGG$',
-            'TGG$',
-            'GC',
-            'TCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'C',
-            'ATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CGCTGG$',
-            'TGG$',
-            'CGC',
-            'TGG$',
-            'C',
-            'AGGGTGTATAACGCCGCCGCTGG$',
-            'TCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'G',
-            'T',
-            'ACCAGCAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'ATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CG',
-            'GG$',
-            'TACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'C',
-            'T',
-            '$',
-            'CATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'GGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'CAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'C',
-            'GGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'AGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CGCTGG$',
-            'TGG$',
-            'CGC',
-            'GG$',
-            'TACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'C',
-            'T',
-            '$',
-            'AGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'GGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'CTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'AGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TGTATAACGCCGCCGCTGG$',
-            'G',
-            'TGTATAACGCCGCCGCTGG$',
-            'G',
-            'AGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'GGGTGTATAACGCCGCCGCTGG$',
-            'TAACGCCGCCGCTGG$',
-            'A',
-            'AACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'ATAACGCCGCCGCTGG$',
-            'TACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'G',
-            'T',
-            'ACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CGTAGGGTGTATAACGCCGCCGCTGG$',
-            'G',
-            'T',
-            'T',
-            'CGCCGCCGCTGG$',
-            'GGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'A',
-            'AGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'AGCAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'C',
-            'C',
-            'GGGTGTATAACGCCGCCGCTGG$',
-            'TAACGCCGCCGCTGG$',
-            'A',
-            'ATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'AGGGTGTATAACGCCGCCGCTGG$',
-            'TCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'G',
-            'T',
-            'TACCAGCAGTGAACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'C',
-            'AACATGGGAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            '$',
-            'GAGGACCAGTAAGGAAGGCTTACCCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'G',
-            'ATAACGCCGCCGCTGG$',
-            'GTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'TACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'G',
-            'T',
-            'AGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'CCTCGATGTGTTACAGACTCGTTCGTAGGGTGTATAACGCCGCCGCTGG$',
-            'AC',
-            'T',
-            'T',
-            'CGTAGGGTGTATAACGCCGCCGCTGG$'
-        ]
         
-        print ('Expected = {0}, actual={1}'.format(len(Expected),len(Edges)))
-        expected = iter(sorted(Expected))
-        edges    = iter(sorted(Edges))
-        exp      = next(expected)
-        ed       = next(edges)
-        while exp != '-' and ed !='-':
-            if exp<ed:
-                print('{0},{1}'.format(exp,'-'))
-                exp = next(expected,'-')           
-            elif ed<exp:
-                print('{0},{1}'.format('-',ed))
-                ed = next(edges,'-') 
-            else:
-                exp = next(expected,'-')
-                ed = next(edges,'-')            
+    if args.extra:
+        strings  = read_strings('data/SuffixTreeConstruction.txt')
+        Edges    = ConstructSuffixTreeEdges(strings[1])   
+        Expected = strings[3:]
+        
+    compare_edges(Edges,Expected) 
+            
             
