496800a5e576d4a4c44282b2aac6f4e69208cf7f
Simon Crase <simon@greenweaves.nz>
2020-10-28 06:33:46 +1300

    #40 - Fixed typos

--- BA9E.py
+++ BA9E.py
@@ -13,7 +13,7 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
-# BA9D Find the Longest Repeat in a String
+# BA9E Find the Longest Substring Shared by Two Strings
 
 import argparse
 from helpers import read_strings
