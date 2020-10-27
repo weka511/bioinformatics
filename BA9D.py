d45696937340c75ce73599263dee806d102226d4
Simon Crase <simon@greenweaves.nz>
2020-10-28 10:38:43 +1300

    #40 - passes extra, but not raosalind data

--- BA9D.py
+++ BA9D.py
@@ -17,7 +17,8 @@
 
 import argparse
 from helpers import read_strings
-from snp import SuffixTree   
+from snp import SuffixTree
+import sys
 import time
 
 def FindLongestRepeat(string):
@@ -28,13 +29,21 @@ def FindLongestRepeat(string):
     tree.build(string)
     edges = sort_by_length(tree.collectEdges(),reverse=True)
     for edge in edges:
-        index = string.find(edge)
-        if index==-1: break
-        if string.find(edge,index+1)>-1:
-            return edge
-
+        #if 69 < len(edge) and len(edge)<80:
+            #print (len(edge),edge)
+        index1 = string.find(edge)
+        if index1==-1: continue
+        index2 = string.find(edge,index1+1)
+        if index2==-1: continue
+        index = index1
+        while string[index1-1]==string[index2-1]:
+            index1-=1
+            index2-=1
+        prefix=string[index1:index]
+        return prefix+edge
 
 if __name__=='__main__':
+    sys.setrecursionlimit(1500)
     start = time.time()
     parser = argparse.ArgumentParser('BA9D Find the Longest Repeat in a String')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
@@ -45,9 +54,22 @@ if __name__=='__main__':
         print (FindLongestRepeat('ATATCGTTTTATCGTT'))
         
     if args.extra:
-        pass
+        Input,Expected  = read_strings('data/LongestRepeat.txt',init=0)
+        print (Input[0])       
+        Actual = FindLongestRepeat(Input[0])
+        print (len(Expected[0]),len(Actual))
+        print (Expected[0])
+        print (Actual)
+
     if args.rosalind:
-        pass
+        Input = read_strings(r'C:\Users\Simon\Downloads\rosalind_ba9d.txt')
+        print (Input[0])
+        tree = SuffixTree()
+        Result = FindLongestRepeat(Input[0])
+        print (Result)
+        with open('foo.txt','w') as f:
+            f.write(f'{Result}\n')
+            
     elapsed = time.time()-start
     minutes = int(elapsed/60)
     seconds = elapsed-60*minutes
