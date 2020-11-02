2c78f9cb48628b146507df59a9190f73dceca0f4
Simon Crase <simon@greenweaves.nz>
2020-11-03 11:36:01 +1300

    Removed version that had been changed wrongly

--- template.py
+++ template.py
@@ -13,36 +13,28 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-# FIBO Fibonacci Numbers
+#  XXXX ................................
 
 import argparse
 import os
 import time
 from helpers import read_strings
 
-def fibo(N):
-    F     = []     
+  
 if __name__=='__main__':
-    F     = []
     start = time.time()
-    parser = argparse.ArgumentParser('FIBO Fibonacci Numbers')
+    parser = argparse.ArgumentParser('....')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
     parser.add_argument('--extra',    default=False, action='store_true', help='process extra dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
-        F[0] = 0
-        F[1] = 1
+        pass
         
     
     if args.extra:
         Input,Expected  = read_strings('data/....txt',init=0)
-        trie = Trie(Input)
-        Actual = None
-        Expected.sort()
-        print (len(Expected),len(Actual))
-        diffs = [(e,a) for e,a in zip(Expected,Actual) if e!=a]
-        print (diffs)
+        ...
   
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
