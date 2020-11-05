f41da1a01f8ecdd2c4b50f21c499b397bbb20623
Simon Crase <simon@greenweaves.nz>
2020-11-05 14:09:43 +1300

    Converted ctbl.py to  standard form

--- ctbl.py
+++ ctbl.py
@@ -1,22 +1,26 @@
-# Copyright (C) 2017 Greenweaves Software Pty Ltd
+# Copyright (C) 2017-2020 Greenweaves Software Limited
 
-# This is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+#   This program is free software: you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation, either version 3 of the License, or
+#   (at your option) any later version.
 
-# This software is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
 
-# You should have received a copy of the GNU General Public License
-# along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>
- 
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
 # ctbl Creating a Character Table  http://rosalind.info/problems/ctbl/
 
-from Bio import Phylo
-import random
+import argparse
+import os
+import time
+from   helpers import read_strings
+from   Bio import Phylo
+from   io import StringIO
 
 def CharacterTable(tree):
     def create_character(split_species):
@@ -32,7 +36,31 @@ def CharacterTable(tree):
     # we iterate over all Clades except the root
     return [create_character([spec.name for spec in split.find_elements(terminal=True)]) for split in clades[1:]]
 
-if __name__=='__main__': 
-    tree = Phylo.read('c:/Users/Weka/Downloads/rosalind_ctbl(4).txt', 'newick')
-    for ch in CharacterTable(tree):
-        print (ch)
+def create_tree(treedata):
+    handle   = StringIO(treedata)
+    return Phylo.read(handle, "newick") 
+
+if __name__=='__main__':
+    start = time.time()
+    parser = argparse.ArgumentParser('....')
+    parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
+    args = parser.parse_args()
+    if args.sample:       
+        for ch in CharacterTable(create_tree('(dog,((elephant,mouse),robot),cat);')):
+            print (ch) 
+        
+    if args.rosalind:
+        tree = Phylo.read(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt', 'newick')
+
+        Result = CharacterTable(tree)
+        with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
+            for line in Result:
+                print (line)
+                f.write(f'{line}\n')
+                
+    elapsed = time.time()-start
+    minutes = int(elapsed/60)
+    seconds = elapsed-60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s') 
+    
