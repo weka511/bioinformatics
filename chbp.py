878b79f067c16c7ec554fb63ff94b0843dd959b0
Simon Crase <simon@greenweaves.nz>
2020-11-07 13:08:02 +1300

    #44 WIP for chbp

--- chbp.py
+++ chbp.py
@@ -23,15 +23,16 @@ import numpy as np
 
 class Clade:
     seq = 0
-    def __init__(self,symbols,character):
+    def __init__(self,symbols,character,weight=1):
         self.symbols   = symbols
         self.character = character
         self.seq       = Clade.seq
+        self.weight    = weight
         Clade.seq      += 1
     def distance(self,other):
-        return sum([abs(a-b) for a,b in zip(self.character,other.character)] )
+        return sum([abs(a/self.weight-b/other.weight) for a,b in zip(self.character,other.character)] )
     def __str__(self):
-        return f'{self.seq}: {self.symbols} {self.character}'
+        return f'{self.seq}: {self.symbols} {self.character} {self.weight}]'
     
 def chbp(species,character_table):
     clades = {}
@@ -45,12 +46,16 @@ def chbp(species,character_table):
         i,j       = keys[index]
         clade1    = clades[i]
         clade2    = clades[j]
-        merged    = Clade(f'({clade1.symbols},{clade2.symbols})',[(a+b)/2 for a,b in zip(clade1.character,clade2.character)])
+        merged    = Clade(f'({clade1.symbols},{clade2.symbols})',
+                          [a+b for a,b in zip(clade1.character,clade2.character)],
+                          weight=clade1.weight+clade2.weight)
         print (f'Merged {clade1.symbols} and {clade2.symbols}=>{merged}')
         clades[merged.seq] = merged
         del clades[clade1.seq]
-        del clades[clade2.seq]
-        
+        del clades[clade2.seq] 
+    #v = list(clades.values())
+    return list(clades.values())[0]
+
 def expand_as_ints(s):
     return [int(c) for c in s]
 
@@ -58,23 +63,35 @@ if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('....')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--abcde',   default=False, action='store_true', help="process Jonathan Dursi's dataset")
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
-        print (chbp(
+        clade = chbp(
             ['cat', 'dog', 'elephant', 'mouse', 'rabbit', 'rat'],
             [expand_as_ints('011101'),
              expand_as_ints('001101'),
-             expand_as_ints('001100')] ))
+             expand_as_ints('001100')] )
+        print (f'{clade.symbols};')
+        
+    if args.abcde:
+        clade = chbp(
+            ['a', 'b', 'c', 'd', 'e'],
+            [expand_as_ints('00000'),
+             expand_as_ints('00110'),
+             expand_as_ints('01000'),
+             expand_as_ints('01110'),
+             expand_as_ints('01111')] )
+        print (f'{clade.symbols};')        
   
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
- 
-        Result = None
-        print (Result)
+        species = Input[0].split()
+        character_table = [expand_as_ints(row) for row in Input[1:]]
+        clade = chbp(species,character_table)
+        print (f'{clade.symbols};')
         with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
-            for line in Result:
-                f.write(f'{line}\n')
+            f.write(f'{clade.symbols};\n')
                 
     elapsed = time.time()-start
     minutes = int(elapsed/60)
