80604c98899cc779e5f0bdd0936e4b03f776f48a
Simon Crase <simon@greenweaves.nz>
2020-11-10 11:47:09 +1300

    #44 passed sample and rosalind tests

--- qrt.py
+++ qrt.py
@@ -13,37 +13,65 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#  XXXX ................................
+#  qrt Incomplete Characters
 
 import argparse
 import os
 import time
 from helpers import read_strings
 
-  
+def tuples(n):
+    for i in range(n):
+        for j in range(n):
+            if i==j: continue
+            for k in range(n):
+                if k in [i,j]: continue
+                for l in range(n):
+                    if l in [i,j,k]: continue
+                    if i<j and k<l and i<k:
+                        yield i,j,k,l
+
+def qrt(taxa,characters):
+    def isConsistent(selector):
+        for char in characters:
+            character = [char[i] for i in selector]
+            if any(c is None for c in character): continue
+            if character[0]==character[1] and character[2]==character[3] and character[0]!=character[2]: return True
+        return False
+    
+    for (i,j,k,l) in tuples(len(taxa)):     
+        selector =  [i,j,k,l]
+        if isConsistent(selector):
+            yield [taxa[m] for m in selector]
+
+
+def expand_character(s):
+    return [int(c) if c.isdigit() else None for c in s]
+
+def format(quartet):
+    return f'{{{quartet[0]}, {quartet[1]}}} {{{quartet[2]}, {quartet[3]}}}'
+
 if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('....')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
-    parser.add_argument('--extra',    default=False, action='store_true', help='process extra dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
-        pass
-        
-    
-    if args.extra:
-        Input,Expected  = read_strings('data/....txt',init=0)
-        ...
+        for quartet in qrt(
+                        ['cat', 'dog', 'elephant', ' ostrich', 'mouse', 'rabbit', 'robot'],
+                        [expand_character() for character in ['01xxx00', 'x11xx00', '111x00x']]):
+            print (format(quartet))
   
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
- 
-        Result = None
-        print (Result)
+        
         with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
-            for line in Result:
-                f.write(f'{line}\n')
+            for quartet in qrt(Input[0].split(),
+                               [expand_character(character) for character in Input[1:]]):
+                Result = format(quartet)
+                print (Result)
+                f.write(f'{Result}\n')
                 
     elapsed = time.time()-start
     minutes = int(elapsed/60)
