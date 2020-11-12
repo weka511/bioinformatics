9681d4112ffa1bb6d81544a9852eed5c0013a817
Simon Crase <simon@greenweaves.nz>
2020-11-13 08:21:36 +1300

    Removed unused code from template

--- template.py
+++ template.py
@@ -18,24 +18,20 @@
 import argparse
 import os
 import time
-from helpers import read_strings
+from   helpers import read_strings
 
   
 if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('....')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
-    parser.add_argument('--extra',    default=False, action='store_true', help='process extra dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
         pass
         
     
-    if args.extra:
-        Input,Expected  = read_strings('data/....txt',init=0)
-        ...
-  
+
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
  
