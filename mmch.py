afca74cddbea62916d37ba9af164841affcca812
Simon Crase <simon@greenweaves.nz>
2020-11-20 17:31:33 +1300

    Closed #55

--- mmch.py
+++ mmch.py
@@ -19,10 +19,13 @@ import argparse
 import os
 import time
 from   helpers import read_strings
-
+from math import factorial
+import decimal 
 def mmch(s):
+    def comb(n,r):
+        return factorial(n) // factorial(n-r)    
     def prod(a,b):
-        return max(1, counts[a] * counts[b])
+        return int(comb(max(counts[a],counts[b]), min(counts[a],counts[b])))
     
     counts = {'A':0,'C':0,'G':0,'U':0}
     for c in s:
@@ -30,20 +33,26 @@ def mmch(s):
     return prod('A','U') * prod('C','G')
 
 if __name__=='__main__':
+    decimal.getcontext().prec=100
     start = time.time()
     parser = argparse.ArgumentParser('....')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--sample2',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--sample3',   default=False, action='store_true', help='process sample dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
         print (mmch('AUGCUUC'))
-        
+    if args.sample2:
+        print (mmch('CAGCGUGAUCAC')) 
+    if args.sample3:
+        print (mmch('CAGCGUGAUCACCAGCGUGAUCAC'))     
     
 
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
  
-        Result = mmch(Input[1])
+        Result = mmch(''.join(s for s in Input[1:]))
         print (Result)
         with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
             f.write(f'{Result}\n')
