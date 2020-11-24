d3cfd311699a3e99cab488235aa44100e1b3fc36
Simon Crase <simon@greenweaves.nz>
2020-11-24 15:27:32 +1300

    #70: WIP checkin

--- rnas.py
+++ rnas.py
@@ -21,28 +21,25 @@ import time
 from   helpers import read_strings
 from   combinatorics import catmotz, partition
 
+            
+def valid_bonds(seq,indices):
+    j  = min(indices)
+    for k in range(j+4,max(indices)+1):
+        if seq[j] + seq[k]==0 or (seq[j]<0 and seq[k]<0): # Bond or wobble bond
+            yield (j,k)
+            
 def count_wobble(seq):
     def wrapped_count(indices):
-        def valid_bonds():
-            j      = min(indices)
-            for k in range(j+4,max(indices)+1):
-                if seq[j] + seq[k]==0 or (seq[j]<0 and seq[k]<0): # Bond or wobble bond
-                    yield (j,k)
-                    
         def count():
             n      = len(indices)
-            if n<2: return 1
-            #j      = min(indices)          
-            count1 = wrapped_count(indices[1:]) #  If first node is not involved in a matching            
-            count2 = 0                            #  If first node is involved in a matching
-            #for k in range(j+4,max(indices)+1):
-                #if seq[j] + seq[k]==0 or (seq[j]<0 and seq[k]<0): # Bond or wobble bond
-            for j,k in valid_bonds():
+            if n<2: return 1       
+            result = wrapped_count(indices[1:]) #  If first node is not involved in a matching            
+                          
+            for j,k in valid_bonds(seq,indices):  #  If first node is involved in a matching
                 I1,I2   = partition(indices,j,k)
-                count21 = wrapped_count(I1)
-                count22 = wrapped_count(I2)
-                count2 += (count21*count22)                
-            return count1 + count2
+                result += wrapped_count(I1) * wrapped_count(I2) 
+            return result
+        
         key = str(indices)
         if not key in cache:
             cache[key] = count()        
@@ -57,10 +54,11 @@ if __name__=='__main__':
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
+        #print (rnas('CGAUGCUAG'))
         print (catmotz('CGAUGCUAG',
-                       counter=count_wobble))        
-        print (catmotz('AUGCUAGUACGGAGCGAGUCUAGCGAGCGAUGUCGUGAGUACUAUAUAUGCGCAUAAGCCACGU',
-                       counter=count_wobble))
+                       counter=count_wobble)) #Expect 12       
+        #print (catmotz('AUGCUAGUACGGAGCGAGUCUAGCGAGCGAUGUCGUGAGUACUAUAUAUGCGCAUAAGCCACGU',
+                       #counter=count_wobble)) # Expect 284850219977421
  
         
     
@@ -68,11 +66,10 @@ if __name__=='__main__':
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
  
-        Result = None
+        Result = catmotz(Input[0], counter=count_wobble)
         print (Result)
         with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
-            for line in Result:
-                f.write(f'{line}\n')
+            f.write(f'{Result}\n')
                 
     elapsed = time.time() - start
     minutes = int(elapsed/60)
