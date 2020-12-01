cf127d77228da33551c4a3d76248b92817f74b24
Simon Crase <simon@greenweaves.nz>
2020-12-02 11:59:09 +1300

    #76 BA9L Implement BWMatching - sample test

--- BA9L.py
+++ BA9L.py
@@ -19,8 +19,46 @@ import argparse
 import os
 import time
 from   helpers import read_strings
+from   snp     import LastToFirst
 
-  
+def BW_Match(LastColumn,Patterns):
+    def contains(LastColumn,symbol,top,bottom):
+        topIndex    = None
+        bottomIndex = None
+        N           = len(LastColumn)
+        for i in range(top,N):
+            if LastColumn[i]==symbol:
+                bottomIndex = i
+                break
+        for i in range(bottom,-1,-1):
+            if LastColumn[i]==symbol:
+                topIndex = i
+                break
+            
+        return topIndex,bottomIndex
+    
+    def Match(FirstColumn, LastColumn, Pattern):
+        top    = 0
+        bottom = len(LastColumn)-1
+        while top <= bottom:
+            if len(Pattern) > 0:
+                symbol  = Pattern[-1]
+                Pattern = Pattern[:-1]
+                topIndex,bottomIndex = contains(LastColumn,symbol,top,bottom)
+                if topIndex != None:
+                    top    = LastToFirst(LastColumn,bottomIndex)#topIndex)
+                    bottom = LastToFirst(LastColumn,topIndex)#bottomIndex)
+                    x=0
+                else:
+                    return 0
+            else:
+                return bottom - top + 1
+        return 0
+    
+    FirstColumn = sorted(LastColumn)
+    #print ([LastToFirst(LastColumn,i) for i in range(len(LastColumn))])
+    return [Match(FirstColumn, LastColumn, Pattern) for Pattern in Patterns]
+
 if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('BA9L 	Implement BWMatching ')
@@ -28,7 +66,10 @@ if __name__=='__main__':
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
-        pass
+        #print (BW_Match('smnpbnnaaaaa$a',
+                        #['ana']))        
+        print (BW_Match('TCCTCTATGAGATCCTATTCTATGAAACCTTCA$GACCAAAATTCTCCGGC',
+                        ['CCT', 'CAC', 'GAG', 'CAG', 'ATC']))
         
     
 
