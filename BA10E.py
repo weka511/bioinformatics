393bba0b72341ab842f3424f3151f65680b99e97
Simon Crase <simon@greenweaves.nz>
2020-12-03 21:03:12 +1300

    #83 Calculating Profile

--- BA10E.py
+++ BA10E.py
@@ -24,14 +24,30 @@ def ConstructProfileHMM(theta,Alphabet,Alignment):
     def CreateState(i):
         return [f'{c}{i+1}' for c in 'MDI']
     
-    def CreateAllStates():
-        return ['S','I0'] + flatten([CreateState(i) for i in range(m)]) + ['E']
-    
+    def CreateProfile():
+        Profile = [[0]*m for k in range(K)]
+        for i in range(n):
+            for j in range(m):
+                ch    = Alignment[i][j]
+                index = [k for k in range(len(Alphabet)) if Alphabet[k]==ch]
+                if len(index)==1:
+                    k = index[0]
+                    Profile[k][j]+=1
+                    
+        for i in range(m):
+            total = sum([Profile[k][i] for k in range(K)])
+            for k in range(K):
+                Profile[k][i] /= total
+                 
+        return Profile
     m          = len(Alignment[0])
-    States     = CreateAllStates()
+    n          = len(Alignment)
+    K          = len(Alphabet)
+    Profile    = CreateProfile()
+    States     = ['S','I0'] + flatten([CreateState(i) for i in range(m)]) + ['E']
     Transition = []
-    Emission   = []
-    x=0
+    Emission   = [[0]*m]
+    return Transition, Emission
 
 if __name__=='__main__':
     start = time.time()
@@ -41,20 +57,32 @@ if __name__=='__main__':
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
-        ConstructProfileHMM(0.289,
-                            ['A',   'B',   'C',   'D',   'E'],
-                            ['EBA', 'EBD', 'EB-', 'EED', 'EBD', 'EBE','E-D','EBD'])
+        Transition,Emission = ConstructProfileHMM(
+                                   0.289,
+                                   ['A',   'B',   'C',   'D',   'E'],
+                                   ['EBA', 'EBD', 'EB-', 'EED', 'EBD', 'EBE','E-D','EBD'])
         
-    
+        print (Transition)
+        print (Emission)
+               
     if args.extra:
         Input,Expected                         = read_strings(f'data/ProfileHMM.txt',init=0)
-        ConstructProfileHMM(float(Input[0]), Input[2].split(),Input[4:-1]) 
-        
+        Transition,Emission = ConstructProfileHMM(
+                                   float(Input[0]),
+                                   Input[2].split(),
+                                   Input[4:-1]) 
+        print (Transition)
+        print (Emission)
+               
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
  
-        Result = None
-        print (Result)
+        Transition,Emission = ConstructProfileHMM(
+                                   float(Input[0]),
+                                   Input[2].split(),
+                                   Input[4:-1]) 
+        print (Transition)
+        print (Emission)
         with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
             for line in Result:
                 f.write(f'{line}\n')
