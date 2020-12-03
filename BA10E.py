a5e95b2b75ef81363499840b6f5383d930c56b11
Simon Crase <simon@greenweaves.nz>
2020-12-03 20:02:13 +1300

    #83 Downloaded extra data and set up test harness

--- BA10E.py
+++ BA10E.py
@@ -18,20 +18,38 @@
 import argparse
 import os
 import time
-from   helpers import read_strings
+from   helpers import read_strings, flatten
 
-  
+def ConstructProfileHMM(theta,Alphabet,Alignment):
+    def CreateState(i):
+        return [f'{c}{i+1}' for c in 'MDI']
+    
+    def CreateAllStates():
+        return ['S','I0'] + flatten([CreateState(i) for i in range(m)]) + ['E']
+    
+    m          = len(Alignment[0])
+    States     = CreateAllStates()
+    Transition = []
+    Emission   = []
+    x=0
+
 if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('BA10E 	Construct a Profile HMM')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--extra',   default=False, action='store_true', help='process sample dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
-        pass
+        ConstructProfileHMM(0.289,
+                            ['A',   'B',   'C',   'D',   'E'],
+                            ['EBA', 'EBD', 'EB-', 'EED', 'EBD', 'EBE','E-D','EBD'])
         
     
-
+    if args.extra:
+        Input,Expected                         = read_strings(f'data/ProfileHMM.txt',init=0)
+        ConstructProfileHMM(float(Input[0]), Input[2].split(),Input[4:-1]) 
+        
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
  
