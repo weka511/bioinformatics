3dd4434c721f6d619932740e02b6200b4abd1514
Simon Crase <simon@greenweaves.nz>
2020-12-03 16:00:39 +1300

    #82 passed sample and extra tests

--- BA10D.py
+++ BA10D.py
@@ -18,20 +18,44 @@
 import argparse
 import os
 import time
-from   helpers import read_strings
+from   helpers import read_strings,create_hmm_from_strings
 
-  
+def Likelihood(xs,Alphabet,States,Transition,Emission):
+    def calculateproduct_weights(s_source = 1):
+        def product_weight(k,x):
+            return sum([s[-1][l] * Transition[(States[l],k)] * Emission[(k,x)] for l in range(len(States))])
+        
+        s = []  # first index is position, 2nd state
+    
+        s.append([s_source * (1/len(States)) * Emission[(k,xs[0])] for k in States])
+        for x in xs[1:]:
+            s.append([product_weight(k,x) for k in States])
+        return s 
+    return sum(calculateproduct_weights()[-1])
+
+
 if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('BA10D 	Compute the Probability of a String Emitted by an HMM ')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--extra',   default=False, action='store_true', help='process extra dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
-        pass
-        
+        print (Likelihood('xzyyzzyzyy',
+                          ['x', 'y','z'], 
+                          ['A','B'],
+                          {('A','A') : 0.303, ('A','B') :   0.697,
+                           ('B','A') : 0.831, ('B','B') :   0.169, },
+                          {('A','x') : 0.533, ('A','y') :   0.065, ('A','z') : 0.402,
+                           ('B','x') : 0.342, ('B','y') :   0.334,  ('B','z') : 0.324, }))
+          
     
-
+    if args.extra:
+        Input,Expected                         = read_strings(f'data/OutcomeLikelihood.txt',init=0)
+        xs,alphabet,States,Transition,Emission = create_hmm_from_strings(Input)
+        print (Likelihood(xs,alphabet,States,Transition,Emission))
+        
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
  
