f50ba690f7fa6d938476ff43c7cfb52eaf62726b
Simon Crase <simon@greenweaves.nz>
2020-12-13 18:04:41 +1300

    #58 Rewrote to handle 'n' strings

--- mult.py
+++ mult.py
@@ -19,12 +19,68 @@ import argparse
 import os
 import time
 from   helpers import read_strings
-from   numpy   import argmax,ravel
+from   numpy   import argmax,ravel,zeros
 
-def score(chars,
-          match       = 0,
-          mismatch    = -1):    
+def score(chars, match    = 0, mismatch = -1):    
     return sum(match if chars[i]==chars[j] else  mismatch for i in range(len(chars)) for j in range(i+1,len(chars)))
+
+def FindHighestScoringMultipleSequenceAlignment(Strings,score = score):
+    def indices(ns=[len(S)+1 for S in Strings]):
+        ii = [0]*len(ns)
+        N  = 1
+        for n in ns:
+            N*=n
+        for _ in range(N-1):          
+            for j in range(len(ns)-1,-1,-1):
+                ii[j] += 1
+                if ii[j]==ns[j]:
+                    ii[j] = 0
+                else:
+                    break
+            yield tuple(ii)
+            
+    def create_moves(m,options=[0,-1]):
+        def create_raw_moves(m):
+            if m==1:
+                return [[o] for o in options]
+            else:
+                return [[o] + c for o in options for c in create_raw_moves(m-1)]
+        return [move for move in tuple(create_raw_moves(m)) if len([x for x in move if x!=0])>0]
+     
+    def add(u,v):
+        return tuple([a + b for (a,b) in zip(list(u),list(v))])
+    
+    def build_matrix():
+        def calculate_scores(i):
+            def get_score(move):
+                previous = add(i,move)
+                scorable = []
+                for j in range(len(move)):
+                    scorable.append(Strings[j][previous[j]]  if move[j]<0 else '-')
+                return s[previous] + score(scorable)
+            return [get_score(move) for move in moves]  # filter out -ve coordinates
+        
+        s     = zeros([len(S)+1 for S in Strings],dtype=int)
+        path  = {}
+        m     = len(Strings)
+        moves = create_moves(m)
+        for index_set in indices():
+            scores           = calculate_scores(index_set)
+            index_best_score = argmax(scores)
+            s[index_set]     = scores[index_best_score]
+            path[index_set]  = moves[index_best_score]
+        return s,path,moves
+    
+    def backtrack(history):    
+        s,path,moves  = history
+        last_position = tuple([len(S) for S in Strings])
+        myscore       = s[last_position]
+        x=0
+        Alignments   = [[] for S in Strings]
+        return myscore,Alignments
+    return backtrack(build_matrix())
+
+
 
 def score_strings(s,t,u,v):
     return sum(score([s[i],t[i],u[i],v[i]]) for i in range(len(s)))
@@ -36,7 +92,7 @@ def MultipleAlignment(Strings,
     s = [[[[0 for l in range(len(Strings[3])+1)]     \
               for k in range(len(Strings[2])+1)]     \
               for j in range(len(Strings[1])+1)]     \
-              for i in range(len(Strings[0])+1) ]
+              for i in range(len(Strings[0])+1)]
     
     moves = [
         [-1, -1, -1, -1],
@@ -49,9 +105,9 @@ def MultipleAlignment(Strings,
         [-1, -1,  0,  0],
         [-1,  0, -1,  0],
         [ 0, -1, -1,  0],
-        [ 0,  0,  0, -1],
+        [ -1,  0,  0, -1],
         [ 0, -1,  0, -1],
-        [ 0,  0, -1, -1],
+        [ 0,  0, -1, -1],    
         
         [-1,  0,  0,  0],
         [0,  -1,  0,  0],
@@ -80,7 +136,7 @@ def MultipleAlignment(Strings,
                         s[i-1][j][k][l-1]   + score([Strings[0][i-1], '-',             '-',             Strings[3][l-1]]),
                         s[i][j-1][k][l-1]   + score(['-',             Strings[1][j-1], '-',             Strings[3][l-1]]),                      
                         s[i][j][k-1][l-1]   + score(['-',             '-',             Strings[2][k-1], Strings[3][l-1]]),
-                        
+                    
                                                 
                         s[i-1][j][k][l]       + score([Strings[0][i-1], '-',             '-',             '-']),
                         s[i][j-1][k][l]       + score(['-',             Strings[1][j-1], '-',             '-']),
@@ -119,14 +175,20 @@ if __name__=='__main__':
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
-        print (score_strings('ATAT-CCG',
-                             '-T---CCG',
+        FindHighestScoringMultipleSequenceAlignment(['ATATCCG','TCCGA','ATGTACTG'],
+                                                    score=lambda X: 1 if X[0]==X[1] and X[1]==X[2] and X[0]!='-' else 0)
+        print (FindHighestScoringMultipleSequenceAlignment(['ATATCCG',
+                             'TCCG',
                              'ATGTACTG',
-                             'ATGT-CTG'))
-        print (MultipleAlignment(['ATATCCG',
-                                  'TCCG',
-                                  'ATGTACTG',
-                                  'ATGTCTG']))
+                             'ATGTCTG']))
+        #print (score_strings('ATAT-CCG',
+                             #'-T---CCG',
+                             #'ATGTACTG',
+                             #'ATGT-CTG'))
+        #print (MultipleAlignment(['ATATCCG',
+                                  #'TCCG',
+                                  #'ATGTACTG',
+                                  #'ATGTCTG']))
         
     
 
