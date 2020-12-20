d198a39d9aa890cee33b8e888ad95de926b233a2
Simon Crase <simon@greenweaves.nz>
2020-12-20 17:49:33 +1300

    #49 Added extra test

--- rsub.py
+++ rsub.py
@@ -72,6 +72,7 @@ if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('RSUB  	Identifying Reversing Substitutions')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--extra',   default=False, action='store_true', help='process extra dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
@@ -92,7 +93,24 @@ if __name__=='__main__':
                              'AATTC']):
             print(format(reverse))
         
-     
+    if args.extra: #Elmar Hitz's example from Questions
+        for reverse in rsub('(((ostrich,cat)rat,mouse)dog,elephant)robot;',
+                            ['>robot',
+                             'AATTG',
+                             '>dog',
+                             'GGGCA',
+                             '>mouse',
+                             'AAGAC',
+                             '>rat',
+                             'GTTGT',
+                             '>cat',
+                             'AAGGC',
+                             '>ostrich',
+                             'GTGTC',
+                             '>elephant',
+                             'AATTC']):
+            print(format(reverse))
+                    
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
  
