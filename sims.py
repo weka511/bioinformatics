d32b3bddc63f118c01ad171ec8b931a016221a1d
Simon Crase <simon@greenweaves.nz>
2020-12-24 19:37:54 +1300

    #62 Converted sims to standard form

--- sims.py
+++ sims.py
@@ -15,8 +15,12 @@
 #
 # SIMS Finding Mutated Motifs
 
+import argparse
+import os
+import time
+from   helpers import read_strings
 import numpy as np
-
+from  fasta import FastaContent
 def get_score(a,b, match=1,mismatch=-1):
     return match if a==b else mismatch
 
@@ -81,11 +85,34 @@ def sims(s,t, match=1,mismatch=-1):
     return (max(scores[-1]), ''.join(s_match[::-1]), ''.join(t_match[::-1]))
 
 if __name__=='__main__':
-     
-    score,s1,t1 = sims('GCAAACCATAAGCCCTACGTGCCGCCTGTTTAAACTCGCGAACTGAATCTTCTGCTTCACGGTGAAAGTACCACAATGGTATCACACCCCAAGGAAAC',
-                       'GCCGTCAGGCTGGTGTCCG')
-    print (score)
-    print (len(s1),s1)
-    print (len(t1),len('GCCGTCAGGCTGGTGTCCG'),t1)
-    print (score_string(s1,t1))
+    start = time.time()
+    parser = argparse.ArgumentParser('KSIM Finding All Similar Motifs')
+    parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
+    args = parser.parse_args()
+    if args.sample:
+        score,s1,t1 = sims('GCAAACCATAAGCCCTACGTGCCGCCTGTTTAAACTCGCGAACTGAATCTTCTGCTTCACGGTGAAAGTACCACAATGGTATCACACCCCAAGGAAAC',
+                           'GCCGTCAGGCTGGTGTCCG')
+        print (score)
+        print (len(s1),s1)
+        print (len(t1),len('GCCGTCAGGCTGGTGTCCG'),t1)
+        print (score_string(s1,t1))
+        
+    if args.rosalind:
+        Input       = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
+        fc          = FastaContent(Input)
+        score,s1,t1 = sims(fc[0][1],fc[1][1])
+        with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
+            print (f'{score}')
+            f.write(f'{score}\n')
+            print (f'{s1}')
+            f.write(f'{s1}\n')
+            print (f'{t1}')
+            f.write(f'{t1}\n')                
+                
+    elapsed = time.time() - start
+    minutes = int(elapsed/60)
+    seconds = elapsed - 60*minutes
+    print (f'Elapsed Time {minutes} m {seconds:.2f} s')    
+
        
