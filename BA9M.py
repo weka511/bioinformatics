8bd0d44c735a502fabf21d7f3dcf5f1a1269295d
Simon Crase <simon@greenweaves.nz>
2021-01-04 17:37:41 +1300

    Added Extra test

--- BA9M.py
+++ BA9M.py
@@ -26,6 +26,7 @@ if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('BA9M 	Implement BetterBWMatching ')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
+    parser.add_argument('--extra',    default=False, action='store_true', help='process extra dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
@@ -33,7 +34,20 @@ if __name__=='__main__':
                                 ['ACC', 'CCG', 'CAG']))
         
     
-
+    if args.extra:
+        Input,Expected  = read_strings(f'data/BetterBWMatching.txt',init=0)
+        Expected = [int(s) for s in Expected[0].split()]
+        Result   = BetterBWMatching(Input[0],Input[1].split())
+        if len(Result)!=len(Expected):
+            print (f'Mismatched lengths {len(Result)} {len(Expected)}')
+            
+        mismatches = 0
+        for i in range(min(len(Result),len(Expected))):
+            if (Result[i]!=Expected[i]):
+                print (f'{i} {Expected[i]} {Result[i]}')
+                mismatches += 1
+        print (f'{mismatches} mismatches')
+            
     if args.rosalind:
         Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
  
