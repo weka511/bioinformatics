8bb3fc4924cad272fab80f2bc70c71e0b35e79ff
Simon Crase <simon@greenweaves.nz>
2021-01-21 12:36:45 +1300

    #67 Passes sample but not rosalind

--- ctea.py
+++ ctea.py
@@ -19,7 +19,9 @@ import argparse
 import os
 import time
 from   helpers import read_strings
-from   align import edta
+from   align import edit
+from   numpy import argmin
+from   Bio import SeqIO
 
 # ctea
 #
@@ -27,9 +29,27 @@ from   align import edta
 #
 # Return: The total number of optimal alignments of s and t with respect to edit alignment score, modulo 134,217,727.
 
-def ctea(s,t):
-    d,s1,t1 = edta(s,t)
-    x=0
+def ctea(s,t,indel_cost=1,replace_cost=lambda a,b: 1, mod=134217727):
+ 
+    def count(s,t,matrix):
+        m       = len(matrix)    - 1
+        n       = len(matrix[0]) - 1
+        product = 1
+        
+        while m>0 and n>0:
+            moves        = [(m-1,n),(m,n-1),(m-1,n-1)]
+            scores       = [matrix[m-1][n]+indel_cost,
+                            matrix[m][n-1]+indel_cost,
+                            matrix[m-1][n-1] + (0 if s[m-1]==t[n-1] else replace_cost(s[m-1],t[n-1]))]
+            index        = argmin(scores)
+            alternatives = len([score for score in scores if score==scores[index]])
+            product     *= alternatives
+            product     %= mod
+            m,n          = moves[index]
+        return product
+    _,matrix = edit(s,t,indel_cost,replace_cost)
+    return count([s0 for s0 in s], [t0 for t0 in t],matrix)
+    
 if __name__=='__main__':
     start = time.time()
     parser = argparse.ArgumentParser('CTEA Counting Optimal Alignments')
@@ -39,16 +59,16 @@ if __name__=='__main__':
     if args.sample:
         print (ctea('PLEASANTLY','MEANLY'))
         
-    
-
     if args.rosalind:
-        Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
+        inFile = open(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt','r')
+        strings = []
+        for record in SeqIO.parse(inFile,'fasta'):
+            strings.append(str(record.seq))        
  
-        Result = None
+        Result = ctea(strings[0],strings[1])
         print (Result)
         with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
-            for line in Result:
-                f.write(f'{line}\n')
+            f.write(f'{Result}\n')
                 
     elapsed = time.time() - start
     minutes = int(elapsed/60)
