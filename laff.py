ed6baeecebf42be35ddb95ab8eca8c705d2a34a9
Simon Crase <simon@greenweaves.nz>
2021-01-23 11:22:37 +1300

    #63 Use numpy arrays

--- laff.py
+++ laff.py
@@ -1,4 +1,4 @@
-#   Copyright (C) 2020 Greenweaves Software Limited
+#   Copyright (C) 2020-2021 Greenweaves Software Limited
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -18,21 +18,24 @@
 import argparse
 import os
 import time
-from   helpers import read_strings
-from   numpy import argmax
-from   fasta import FastaContent
+from   helpers   import read_strings
+from   numpy     import argmax,zeros
+from   fasta     import FastaContent
 from   Bio.Align import substitution_matrices
-from   align import reverse
+from   align     import reverse
+from   Bio       import SeqIO
+# laff
+#
+# Local Alignment with Affine Gap Penalty
 
 def laff(s,t,replace_score=substitution_matrices.load("BLOSUM62"),sigma=11,epsilon=1):
     def score_pair(a,b):
-        return replace_score[(a,b)] if (a,b) in replace_score else replace_score[(b,a)]
+        return int(replace_score[(a,b)] if (a,b) in replace_score else replace_score[(b,a)])
     def unwind_moves(moves,score,i,j):
         ss = []
         ts = []
     
         while i>0 and j > 0:
-            print (f'{i}-{j}')
             i,j,s0,t0=moves[(i,j)]
             ss.append(s0)
             ts.append(t0)
@@ -40,9 +43,9 @@ def laff(s,t,replace_score=substitution_matrices.load("BLOSUM62"),sigma=11,epsil
     
     m      = len(s)
     n      = len(t)
-    lower  = [[0 for j in range(n+1)] for i in range(m+1)]
-    middle = [[0 for j in range(n+1)] for i in range(m+1)]
-    upper  = [[0 for j in range(n+1)] for i in range(m+1)]
+    lower  = zeros((m+1,n+1),dtype=int)
+    middle = zeros((m+1,n+1),dtype=int)
+    upper  = zeros((m+1,n+1),dtype=int)
     moves  = {}
     
     imax      = -1
@@ -50,23 +53,25 @@ def laff(s,t,replace_score=substitution_matrices.load("BLOSUM62"),sigma=11,epsil
     jmax      = -1
     
     for i in range(1,m+1):
-        print (f'{i}')
+        if i%10==0: print (f'{i} {m}')
         for j in range(1,n+1):
-            lower[i][j]  = max(lower[i-1][j]-epsilon,
-                               middle[i-1][j]-sigma)
-            upper[i][j]  = max(upper[i][j-1]-epsilon,
-                               middle[i][j-1]-sigma)
-            choices      = [lower[i][j], 
-                            upper[i][j], 
-                            middle[i-1][j-1] + score_pair(s[i-1],t[j-1])]
-            index        = argmax(choices)
-            middle[i][j] = choices[index]
-            moves[(i,j)] = [(i-1, j,   s[i-1], '-'),     # Comes from lower
-                            (i-1, j-1, s[i-1], t[j-1]),  # Comes from middle
-                            (i,   j-1, '-',    t[j-1]    # Comes from upper
-                             )][index]
+            lower[i,j]      = max(lower[i-1,j]  - epsilon,
+                                  middle[i-1,j] - sigma)
+            upper[i,j]      = max(upper[i,j-1]  - epsilon,
+                                  middle[i,j-1] - sigma)
+            possible_scores = [lower[i,j], 
+                               upper[i,j], 
+                               middle[i-1,j-1] + score_pair(s[i-1],t[j-1])]
+            possible_moves = [(i-1, j,   s[i-1], '-'   ),  # Comes from lower
+                              (i-1, j-1, s[i-1], t[j-1]),  # Comes from middle
+                              (i,   j-1, '-',    t[j-1])]  # Comes from upper
+            
+            index          = argmax(possible_scores)
+            middle[i,j]    = possible_scores[index]                             
+            moves[(i,j)]   = possible_moves[index]
+            
         j     = argmax(middle[i])
-        score = middle[i][j]
+        score = middle[i,j]
         if score>max_score:
             imax      = i
             jmax      = j
@@ -87,16 +92,14 @@ if __name__=='__main__':
         print (t1)    
         
     if args.rosalind:
-        Input       = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
-        fasta       = FastaContent(Input)
-        _,s         = fasta[0]
-        _,t         = fasta[1]
-        score,s1,t1 = laff(s,t)
+        data = [str(rec.seq) for rec in SeqIO.parse(open(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt','r'),
+                                                         'fasta')]                
+        score,s,t = laff(data[0],data[1])
 
         with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:               
             f.write(f'{score}\n')
-            f.write(f'{s1}\n')
-            f.write(f'{t1}\n')
+            f.write(f'{s}\n')
+            f.write(f'{t}\n')
                 
     elapsed = time.time() - start
     minutes = int(elapsed/60)
