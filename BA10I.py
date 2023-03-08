245e7f2d24aa87e7ca4ccc5428e7c0e079be8cba
Simon Crase <simon@greenweaves.nz>
2023-03-08 15:53:38 +1300

    Started on #87

--- BA10I.py
+++ BA10I.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-#   Copyright (C) 2020 Greenweaves Software Limited
+#   Copyright (C) 2020-2023 Greenweaves Software Limited
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -15,35 +15,34 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#  BA10I 	Implement Viterbi Learning
+'''BA10I 	Implement Viterbi Learning'''
 
-import argparse
-import os
-import time
-from   helpers import read_strings
+from argparse import ArgumentParser
+from os.path  import basename
+from time import time
+
+from  helpers import read_strings
 
 
 if __name__=='__main__':
-    start = time.time()
-    parser = argparse.ArgumentParser('BA10I 	Implement Viterbi Learning ')
+    start = time()
+    parser = ArgumentParser(__doc__)
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
         pass
 
-
-
     if args.rosalind:
-        Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
+        Input  = read_strings(f'data/rosalind_{basename(__file__).split(".")[0]}.txt')
 
         Result = None
         print (Result)
-        with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
+        with open(f'{basename(__file__).split(".")[0]}.txt','w') as f:
             for line in Result:
                 f.write(f'{line}\n')
 
-    elapsed = time.time() - start
+    elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
     print (f'Elapsed Time {minutes} m {seconds:.2f} s')
