1c1542de361a2107cf1afd5f3ca6650ef69bc123
Simon Crase <simon@greenweaves.nz>
2023-03-20 10:00:39 +1300

    #125 A few more tests

--- qrt.py
+++ qrt.py
@@ -14,43 +14,43 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#  qrt Incomplete Characters
+''' QRT Incomplete Characters'''
 
-import argparse
-import os
-import time
-from   helpers import read_strings
-from   phylogeny import qrt
+from argparse import ArgumentParser
+from os.path import basename
+from time import time
 
-def expand_character(s):
-    return [int(c) if c.isdigit() else None for c in s]
+from helpers import read_strings
+from   phylogeny import qrt, expand_character
+
+
 
 def format(quartet):
     return f'{{{quartet[0]}, {quartet[1]}}} {{{quartet[2]}, {quartet[3]}}}'
 
 if __name__=='__main__':
-    start = time.time()
-    parser = argparse.ArgumentParser('....')
+    start = time()
+    parser = ArgumentParser('....')
     parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
     parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
     args = parser.parse_args()
     if args.sample:
         for quartet in qrt(
                         ['cat', 'dog', 'elephant', ' ostrich', 'mouse', 'rabbit', 'robot'],
-                        [expand_character() for character in ['01xxx00', 'x11xx00', '111x00x']]):
+                        [expand_character(character) for character in ['01xxx00', 'x11xx00', '111x00x']]):
             print (format(quartet))
 
     if args.rosalind:
-        Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
+        Input  = read_strings(f'data/rosalind_{basename(__file__).split(".")[0]}.txt')
 
-        with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
+        with open(f'{basename(__file__).split(".")[0]}.txt','w') as f:
             for quartet in qrt(Input[0].split(),
                                [expand_character(character) for character in Input[1:]]):
                 Result = format(quartet)
                 print (Result)
                 f.write(f'{Result}\n')
 
-    elapsed = time.time() - start
+    elapsed = time() - start
     minutes = int(elapsed/60)
     seconds = elapsed - 60*minutes
     print (f'Elapsed Time {minutes} m {seconds:.2f} s')
