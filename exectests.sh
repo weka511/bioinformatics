77319d4547f3cc7cd2d868c436d5b03e0d6ba406
Simon Crase <simon@greenweaves.nz>
2024-05-07 09:13:46 +1200

    Started #144

---
+++ exectests.sh
@@ -0,0 +1,23 @@
+#!
+echo align.py
+./align.py
+echo assemble.py
+./assemble.py
+echo fragile.py
+./fragile.py
+echo graphs.py
+./graphs.py
+echo hmm.py
+./hmm.py
+echo phylogeny.py
+./phylogeny.py
+echo reference_tables.py
+./reference_tables.py
+echo replication.py
+./replication.py
+echo rosalind.py
+./rosalind.py
+echo sequence.py
+./sequence.py
+echo spectrum.py
+./spectrum.py
