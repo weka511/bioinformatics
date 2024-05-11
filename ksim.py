2d3b6ec1f3dc5bfe07449b09897f5336c1c43e20
Simon Crase <simon@greenweaves.nz>
2024-05-12 10:07:28 +1200

    #62 Reformatted code

--- ksim.py
+++ ksim.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-#   Copyright (C) 2020 Greenweaves Software Limited
+#   Copyright (C) 2020-2024 Greenweaves Software Limited
 
 #   This program is free software: you can redistribute it and/or modify
 #   it under the terms of the GNU General Public License as published by
@@ -14,37 +14,38 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
-#  KSIM Finding All Similar Motifs
+'''KSIM Finding All Similar Motifs'''
 
-import argparse
-import os
-import time
-from   helpers import read_strings
-from   numpy   import zeros
-from   snp     import create_trie
+from argparse import ArgumentParser
+from os.path import basename
+from time import time
+from helpers import read_strings
+from numpy import zeros
+from snp import create_trie
 
-# ksim
-#
-# Finding All Similar Motifs
-#
-# Given: A positive integer k,
-#        a DNA string s of length at most 5 kbp representing a motif,
-#        and a DNA string t of length at most 50 kbp representing a genome.
-#
-#       Return: All substrings t' of t such that the edit distance dE(s,t') is less than or equal to k.
-#       Each substring should be encoded by a pair containing its location in t followed by its length.
-#
-# The following code is an attempt to see wther we can execure the Manhattan algorithm for rosalind data
-# within 5 minutes (300 seconds). It looks as if this isn't possible.
-#
-# 4900 iterations along t axis required 456 sec, so all of th will need 3934 sec.
 
 def ksim(k,s,t):
+   '''
+   Finding All Similar Motifs
+
+   Given: A positive integer k,
+            a DNA string s of length at most 5 kbp representing a motif,
+            and a DNA string t of length at most 50 kbp representing a genome.
+
+           Return: All substrings t' of t such that the edit distance dE(s,t') is less than or equal to k.
+           Each substring should be encoded by a pair containing its location in t followed by its length.
+
+     The following code is an attempt to see wther we can execure the Manhattan algorithm for rosalind data
+     within 5 minutes (300 seconds). It looks as if this isn't possible.
+
+     4900 iterations along t axis required 456 sec, so all of th will need 3934 sec.
+   '''
    Trie = create_trie([t])
    x=0
+
 if __name__=='__main__':
-   start = time.time()
-   parser = argparse.ArgumentParser('KSIM Finding All Similar Motifs')
+   start = time()
+   parser = ArgumentParser('KSIM Finding All Similar Motifs')
    parser.add_argument('--sample',   default=False, action='store_true', help='process sample dataset')
    parser.add_argument('--rosalind', default=False, action='store_true', help='process Rosalind dataset')
    args = parser.parse_args()
@@ -55,12 +56,12 @@ if __name__=='__main__':
    if args.rosalind:
       Input  = read_strings(f'data/rosalind_{os.path.basename(__file__).split(".")[0]}.txt')
 
-      with open(f'{os.path.basename(__file__).split(".")[0]}.txt','w') as f:
+      with open(f'{basename(__file__).split(".")[0]}.txt','w') as f:
          for a,b in ksim(int(Input[0]),Input[1],Input[2]):
             print (f'{a} {b}')
             f.write(f'{a} {b}\n')
 
-   elapsed = time.time() - start
+   elapsed = time() - start
    minutes = int(elapsed/60)
    seconds = elapsed - 60*minutes
    print (f'Elapsed Time {minutes} m {seconds:.2f} s')
