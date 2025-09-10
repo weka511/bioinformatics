512ebd45e33efac9b2d4a65818a275d4ae849cc6
Weka <simon@greenweaves.nz>
2025-09-11 07:41:47 +1200

    #164 Created skeleton for file adapter

---
+++ csrc/file-adapter.hpp
@@ -0,0 +1,40 @@
+/**
+ * Copyright (C) 2025 Simon Crase
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should   received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>
+ */
+ 
+#ifndef _FILE_ADAPTER_HPP
+#define _FILE_ADAPTER_HPP
+
+#include <iostream>
+#include <memory>
+#include <string>
+#include <vector>
+#include "problem.hpp"
+
+using namespace std;
+
+
+
+class FileOutput : public OutputAdapter{
+   public:
+	void append(string text);
+	void append(vector<int> counts);
+};
+
+
+
+
+#endif //_FILE_ADAPTER_HPP
