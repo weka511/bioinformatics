79eea916283f9962a64fb93fa914ba2b2c7a9445
Weka <simon@greenweaves.nz>
2025-09-11 14:35:22 +1200

    #46 Created skeleton for QRTD

---
+++ csrc/qrtd.cpp
@@ -0,0 +1,29 @@
+/**
+ * Copyright (C) 2025 Simon Crase
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>
+ */
+
+#include <algorithm>
+#include "qrtd.hpp"
+
+using namespace std;
+
+void QRTD::solve() {
+	auto taxa= get_input(0);
+	auto tree1= get_input(1);
+	auto tree2= get_input(2);
+	// TO DO
+	append("0");
+}
---
+++ csrc/test-qrtd.cpp
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2025 Simon Crase
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>
+ */
+ 
+ #include "catch.hpp"
+ #include "qrtd.hpp"
+ #include "test-adapter.hpp"
+ 
+ TEST_CASE( "QRTD Tests", "[qrtd]" ) {
+	 
+	 SECTION("quarted distance") {
+		QRTD qrtd;
+		TestDatasource datasource;
+		datasource.push_back("A B C D E");
+		datasource.push_back("(A,C,((B,D),E));");
+		datasource.push_back("(C,(B,D),(A,E));");
+		qrtd.attach(&datasource);
+		TestOutput output;
+		qrtd.attach(&output);
+		qrtd.solve();
+		REQUIRE(output.get(0) == "4");
+	 }
+ }
