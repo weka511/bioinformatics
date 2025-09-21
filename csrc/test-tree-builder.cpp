0cb31ecaa66973d6449120874a75644578fe84b2
Weka <simon@greenweaves.nz>
2025-09-22 08:21:01 +1200

    #46 Created skeleton TreeBuilder for constructing trees from Newick parse

---
+++ csrc/test-tree-builder.cpp
@@ -0,0 +1,39 @@
+/**
+ * Copyright (C) 2025 Simon Crase
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>
+ *
+ * (,,(,));                               no nodes are named
+ * (A,B,(C,D));                           leaf nodes are named
+ * (A,B,(C,D)E)F;                         all nodes are named
+ * (:0.1,:0.2,(:0.3,:0.4):0.5);           all but root node have a distance to parent
+ * (:0.1,:0.2,(:0.3,:0.4):0.5):0.0;       all have a distance to parent
+ * (A:0.1,B:0.2,(C:0.3,D:0.4):0.5);       distances and leaf names (popular)
+ * (A:0.1,B:0.2,(C:0.3,D:0.4)E:0.5)F;     distances and all names
+ * ((B:0.2,(C:0.3,D:0.4)E:0.5)F:0.1)A;    a tree rooted on a leaf node (rare)
+ */
+ 
+ #include "catch.hpp"
+
+ #include "tree-builder.hpp"
+ 
+ TEST_CASE( "Tree Builder Tests", "[tree_builder]" ) {
+	Parser parser;
+	
+	 SECTION("Test parser with labels") {
+			shared_ptr<Parser::Tree> tree = parser.parse("(A,B,(C,D));");
+			shared_ptr<TreeBuilder> displayer = make_shared<TreeBuilder>();
+			tree->descend(displayer);
+	 }
+}
---
+++ csrc/tree-builder.hpp
@@ -0,0 +1,33 @@
+/**
+ * Copyright (C) 2025 Simon Crase
+ *
+ * This is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should   received a copy of the GNU General Public License
+ * along with this software.  If not, see <http://www.gnu.org/licenses/>
+ */
+ 
+#ifndef _TREE_BUILDER_HPP
+#define _TREE_BUILDER_HPP
+
+#include "node.hpp"
+#include "newick.hpp"
+
+using namespace std;
+
+class TreeBuilder : public Parser::Visitor {
+	void accept(Parser::NewickNode* node,const int depth);
+	
+	void farewell(Parser::NewickNode * node, const int depth);
+ };
+
+#endif // _TREE_BUILDER_HPP
+
